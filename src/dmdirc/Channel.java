08649a3ee9441ad6b0405476fbcff21735ef54d3
Chris Smith <chris@dmdirc.com>
2007-02-08 01:23:01 +0000

    Add missing source files
    git-svn-id: http://svn.dmdirc.com/trunk@5 00569f92-eb28-0410-84fd-f71c24880f43

---
+++ src/dmdirc/Channel.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2006-2007 Chris Smith, Shane Mc Cormack
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package dmdirc;
+
+/**
+ * The Channel class represents the client's view of the channel. It handles
+ * callbacks for channel events from the parser, maintains the corresponding
+ * ChannelFrame, and handles user input to a ChannelFrame
+ * @author chris
+ */
+public class Channel {
+    
+    /** The parser's pChannel class */
+    private int pChannel;
+    
+    /** The server this channel is on */
+    private Server server;
+    
+    /** The ChannelFrame used for this channel */
+    private int frame;
+    
+    /**
+     * Creates a new instance of Channel
+     * @param server The server object that this channel belongs to
+     * @param pChannel The parser's channel object that corresponds to this channel
+     */
+    public Channel(Server server, int pChannel) {
+        this.pChannel = pChannel;
+        this.server = server;
+        
+        frame = 0;
+        // new ChannelFrame...
+    }
+    
+}
---
+++ src/dmdirc/Server.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2006-2007 Chris Smith, Shane Mc Cormack
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package dmdirc;
+
+import java.util.Vector;
+
+/**
+ * The Server class represents the client's view of a server. It maintains
+ * a list of all channels, queries, etc, and handles parser callbacks pertaining
+ * to the server
+ * @author chris
+ */
+public class Server {
+    
+    /**
+     * Open channels that currently exist on the server
+     */
+    private Vector channels;
+    
+    /**
+     * The ServerFrame corresponding to this server
+     */
+    private ServerFrame frame;
+    
+    /** Creates a new instance of Server */
+    public Server() {
+        
+        channels = new Vector(0,1);
+        
+        ServerManager.getServerManager().registerServer(this);
+        
+        frame = new ServerFrame();
+        
+        MainFrame.getMainFrame().addChild(frame);
+    }
+    
+    /**
+     * Called on destruction, the server unregisters itself with the ServerManager
+     */
+    protected void finalize() throws Throwable {
+        ServerManager.getServerManager().unregisterServer(this);
+    }
+    
+}
