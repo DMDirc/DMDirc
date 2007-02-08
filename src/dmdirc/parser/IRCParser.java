e45b7d929725541c58462a9aebccb3c7f46be50e
Shane Mc Cormack <shane@dmdirc.com>
2007-02-08 07:47:36 +0000

    Added altNickname to myinfo Added default NickInUse handler if one isn't specified Added Quit,Disconnect and SetNickname methods Other misc stuff i think.
    git-svn-id: http://svn.dmdirc.com/trunk@27 00569f92-eb28-0410-84fd-f71c24880f43

--- src/dmdirc/parser/IRCParser.java
+++ src/dmdirc/parser/IRCParser.java
@@ -28,8 +28,12 @@ import java.util.ArrayList;
 
 public class IRCParser implements Runnable {
 
+	public static final int ndInfo = 1;
+//	public static final int ndSomething = 2;
+
 	public class MyInfo {
 		public String sNickname = "IRCParser";
+		public String sAltNickname = "IRC-Parser"; // Alternative nickname, if this fails, we start prepending _ to sNickname
 		public String sRealname = "Java Test IRCParser";
 		public String sUsername = "IRCParser";
 	}
@@ -37,18 +41,26 @@ public class IRCParser implements Runnable {
 	private Socket socket = null;
 	private PrintWriter out = null;
 	private BufferedReader in = null;
+	
+	public MyInfo me = new MyInfo(); // This is what the user wants, nickname here is *not* fact.
 
-	public MyInfo me = new MyInfo();
+	private String sThinkNickname; // This is what we want the nickname to be.
+	private String sConfirmedNickname; // This is what the nickname actually is.
+	private boolean TriedAlt = false;
 
+	private boolean Got001 = false;
+	private boolean Got005 = false;
 	private boolean HasBegan = false;
 	private boolean IsFirst = true;
 
 	// Events
+	public interface IDebugInfo { public void onDebug(IRCParser tParser, int nLevel, String sData); }
 	public interface IMOTDEnd { public void onMOTDEnd(IRCParser tParser); }
 	public interface IDataIn { public void onDataIn(IRCParser tParser, String sData); }
 	public interface IDataOut { public void onDataOut(IRCParser tParser, String sData, boolean FromParser); }
 	public interface INickInUse { public void onNickInUse(IRCParser tParser); }
 	class AllEvents {
+		ArrayList<IDebugInfo> DebugInfo = new ArrayList<IDebugInfo>();
 		ArrayList<IMOTDEnd> EndOfMOTD = new ArrayList<IMOTDEnd>();
 		ArrayList<IDataIn> DataIn = new ArrayList<IDataIn>();
 		ArrayList<IDataOut> DataOut = new ArrayList<IDataOut>();
@@ -68,43 +80,74 @@ public class IRCParser implements Runnable {
 		}
 	}
 
+	public void AddDebugInfo(Object eMethod) { AddCallback(eMethod, cb.DebugInfo); }
+	public void DelDebugInfo(Object eMethod) { DelCallback(eMethod, cb.DebugInfo); }
+	private boolean CallDebugInfo(int level, String data) { 
+		boolean bResult = false;
+		for (int i = 0; i < cb.DebugInfo.size(); i++) {
+			cb.DebugInfo.get(i).onDebug(this, level, data);
+			bResult = true;
+		}
+		return bResult;
+	}
+
 	public void AddMOTDEnd(Object eMethod) { AddCallback(eMethod, cb.EndOfMOTD); }
 	public void DelMOTDEnd(Object eMethod) { DelCallback(eMethod, cb.EndOfMOTD); }
-	private void CallMOTDEnd() { 
+	private boolean CallMOTDEnd() { 
+		boolean bResult = false;
 		for (int i = 0; i < cb.EndOfMOTD.size(); i++) {
 			cb.EndOfMOTD.get(i).onMOTDEnd(this);
+			bResult = true;
 		}
+		return bResult;
 	}
 
 	public void AddDataIn(Object eMethod) { AddCallback((IDataIn)eMethod, cb.DataIn); }
 	public void DelDataIn(Object eMethod) { DelCallback((IDataIn)eMethod, cb.DataIn); }
-	private void CallDataIn(String data) { 
+	private boolean CallDataIn(String data) { 
+		boolean bResult = false;
 		for (int i = 0; i < cb.DataIn.size(); i++) {
 			cb.DataIn.get(i).onDataIn(this, data);
+			bResult = true;
 		}
+		return bResult;
 	}
 
 	public void AddDataOut(Object eMethod) { AddCallback((IDataOut)eMethod, cb.DataOut); }
 	public void DelDataOut(Object eMethod) { DelCallback((IDataOut)eMethod, cb.DataOut); }
-	private void CallDataOut(String data, boolean FromParser) { 
+	private boolean CallDataOut(String data, boolean FromParser) { 
+		boolean bResult = false;
 		for (int i = 0; i < cb.DataOut.size(); i++) {
 			cb.DataOut.get(i).onDataOut(this, data, FromParser);
+			bResult = true;
 		}
+		return bResult;
 	}
 
 	public void AddNickInUse(Object eMethod) { AddCallback(eMethod, cb.NickInUse); }
 	public void DelNickInUse(Object eMethod) { DelCallback(eMethod, cb.NickInUse); }
-	private void CallNickInUse() { 
+	private boolean CallNickInUse() { 
+		boolean bResult = false;
 		for (int i = 0; i < cb.NickInUse.size(); i++) {
 			cb.NickInUse.get(i).onNickInUse(this);
+			bResult = true;
 		}
+		return bResult;
 	}
 
 	// Constructor.
 	public IRCParser () { }
 
+
+	private void ResetState() {
+			TriedAlt = false;
+			Got001 = false;
+			Got005 = false;
+	};
+
 	public void connect(String sHost) throws Exception {
 		try {
+			ResetState();
 			connect(sHost,6667);
 		} catch (Exception e) {
 			throw e;
@@ -132,7 +175,7 @@ public class IRCParser implements Runnable {
 			try {
 				line = in.readLine(); // Blocking :/
 				if (IsFirst) {
-					SendString("NICK "+me.sNickname);
+					SetNickname(me.sNickname);
 					SendString("USER "+me.sUsername.toLowerCase()+" * * :"+me.sRealname);
 					IsFirst = false;
 				}
@@ -220,12 +263,37 @@ public class IRCParser implements Runnable {
 	}
 
 	private void ProcessEndOfMOTD(String sParam,String token[]) { CallMOTDEnd(); }
-	private void ProcessNickInUse(String sParam,String token[]) { CallNickInUse(); }
 
-
-
-	//-------------------------------------------------------------------------
-	public void JoinChannel(String sChannelName) {
-		SendLine("JOIN "+sChannelName);
+	private void ProcessNickInUse(String sParam,String token[]) {
+		if (!CallNickInUse()) {
+			// Manually handle nick in use.
+			CallDebugInfo(ndInfo,"No Nick in use Handler.");
+			if (!Got001) {
+				CallDebugInfo(ndInfo,"Using inbuilt handler");
+				// If this is before 001 we will try and get a nickname, else we will leave the nick as-is
+				if (!TriedAlt) { SetNickname(me.sAltNickname); TriedAlt = true; }
+				else {
+					if (sThinkNickname.equalsIgnoreCase(me.sAltNickname)) { sThinkNickname = me.sNickname; }
+					SetNickname('_'+sThinkNickname);	
+				}
+			}
+		}
 	}
+
+
+	public void JoinChannel(String sChannelName) {
+		SendString("JOIN "+sChannelName);
+	}
+
+	public void SetNickname(String sNewNickName) {
+		sThinkNickname = sNewNickName;
+		SendString("NICK "+sNewNickName);
+	}
+
+	public void Quit(String sReason) { SendString("QUIT :"+sReason); }
+	// Quit and force Disconnection
+	public void Disconnect(String sReason) {
+		Quit(sReason);
+		try { socket.close(); } catch (Exception e) { /* Meh */ };
+	}	
 }
