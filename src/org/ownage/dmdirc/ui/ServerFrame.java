b1d215129b8c46c505bf7103fd97ed62239829f7
Chris Smith <chris@dmdirc.com>
2007-02-26 12:38:46 +0000

    ServerFrame now uses jTextPane
    git-svn-id: http://svn.dmdirc.com/trunk@89 00569f92-eb28-0410-84fd-f71c24880f43

--- src/org/ownage/dmdirc/ui/ServerFrame.java
+++ src/org/ownage/dmdirc/ui/ServerFrame.java
@@ -93,7 +93,7 @@ public class ServerFrame extends javax.swing.JInternalFrame {
      * @param line text to add
      */
     public void addLine(String line) {
-        jTextArea1.append(line+"\n");
+        jTextPane1.setText(jTextPane1.getText()+line+"\n");
     }
     
     /** This method is called from within the constructor to
@@ -103,21 +103,20 @@ public class ServerFrame extends javax.swing.JInternalFrame {
      */
     // <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:initComponents
     private void initComponents() {
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
         jTextField1 = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextPane1 = new javax.swing.JTextPane();
 
         setTitle("Server Frame");
-        jTextArea1.setColumns(20);
-        jTextArea1.setRows(5);
-        jScrollPane1.setViewportView(jTextArea1);
+
+        jScrollPane1.setViewportView(jTextPane1);
 
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
             .add(jTextField1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 394, Short.MAX_VALUE)
-            .add(jScrollPane1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 394, Short.MAX_VALUE)
+            .add(org.jdesktop.layout.GroupLayout.TRAILING, jScrollPane1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 394, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
@@ -132,8 +131,8 @@ public class ServerFrame extends javax.swing.JInternalFrame {
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea jTextArea1;
     private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextPane jTextPane1;
     // End of variables declaration//GEN-END:variables
     
 }
