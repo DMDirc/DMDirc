370c506907de25cfe57ad6e3ae793b24354d92f6
Shane Mc Cormack <shane@dmdirc.com>
2007-03-01 18:34:06 +0000

    Pointless, removed.
    git-svn-id: http://svn.dmdirc.com/trunk@214 00569f92-eb28-0410-84fd-f71c24880f43

--- test/ParserTest.java
+++
@@ -1,9 +0,0 @@
-import java.lang.Thread;
-import uk.org.ownage.dmdirc.parser.*;
-
-public class ParserTest {
-	public static void main(String args[]) {
-		IRCParser ptc = new IRCParser();
-		ptc.DoSelfTest(false);
-	}
-}
