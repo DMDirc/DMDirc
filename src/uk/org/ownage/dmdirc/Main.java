b3a24e73ca117cbeb052fd5ccbbf046a230465d6
Chris Smith <chris@dmdirc.com>
2007-03-06 02:03:42 +0000

    Use the crossplatform L&F until we test properly on others
    git-svn-id: http://svn.dmdirc.com/trunk@406 00569f92-eb28-0410-84fd-f71c24880f43

--- src/uk/org/ownage/dmdirc/Main.java
+++ src/uk/org/ownage/dmdirc/Main.java
@@ -48,7 +48,8 @@ public class Main {
     public static void main(String[] args) {
         
         try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            //UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
         } catch (Exception ex) {
             Logger.error(ErrorLevel.ERROR, ex);
         }
