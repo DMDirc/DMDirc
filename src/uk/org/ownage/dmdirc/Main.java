2db2743adb92e2f41bb9aca4ef50b580d560bd1f
Chris Smith <chris@dmdirc.com>
2007-04-24 21:57:47 +0000

    Tagged 0.3
    git-svn-id: http://svn.dmdirc.com/tags/0.3@900 00569f92-eb28-0410-84fd-f71c24880f43

--- src/uk/org/ownage/dmdirc/Main.java
+++ src/uk/org/ownage/dmdirc/Main.java
@@ -46,7 +46,7 @@ public final class Main {
     /**
      * Stores the current program version.
      */
-    public static final String VERSION = "SVN";
+    public static final String VERSION = "0.3";
     
     /**
      * Prevents creation of main.
