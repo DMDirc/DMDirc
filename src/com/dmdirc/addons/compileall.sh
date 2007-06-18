6b184df8cb19e841ffbcd88704320fe4ba2663ab
Shane Mc Cormack <shane@dmdirc.com>
2007-06-18 19:06:21 +0000

    compileall now also compiles nowplaying plugin SetNickColour now says which nickname is not known.
    git-svn-id: http://svn.dmdirc.com/trunk@1615 00569f92-eb28-0410-84fd-f71c24880f43

--- src/com/dmdirc/addons/compileall.sh
+++ src/com/dmdirc/addons/compileall.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Quick script to recompile all plugins
 rm ./*/*.class
+rm ./*/*/*.class
 cd ../../../
-javac -Xlint:all com/dmdirc/addons/*/*.java
+javac -Xlint:all com/dmdirc/addons/*/*.java com/dmdirc/addons/*/*/*.java
