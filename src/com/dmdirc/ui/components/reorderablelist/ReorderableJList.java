1a86cb39083469d8a7eb2f4987a723fbe5b61edc
Gregory Holmes <greg@dmdirc.com>
2007-07-20 22:37:46 +0000

    Improved reorderable list, can't move items to the bottom of the list
    git-svn-id: http://svn.dmdirc.com/trunk@1740 00569f92-eb28-0410-84fd-f71c24880f43

--- src/com/dmdirc/ui/components/reorderablelist/ReorderableJList.java
+++ src/com/dmdirc/ui/components/reorderablelist/ReorderableJList.java
@@ -23,14 +23,39 @@
 package com.dmdirc.ui.components.reorderablelist;
 
 
+import com.dmdirc.logger.ErrorLevel;
+import com.dmdirc.logger.Logger;
+
+import java.awt.Cursor;
+import java.awt.Point;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DragGestureEvent;
+import java.awt.dnd.DragGestureListener;
+import java.awt.dnd.DragSource;
+import java.awt.dnd.DragSourceDragEvent;
+import java.awt.dnd.DragSourceDropEvent;
+import java.awt.dnd.DragSourceEvent;
+import java.awt.dnd.DragSourceListener;
+import java.awt.dnd.DropTarget;
+import java.awt.dnd.DropTargetDragEvent;
+import java.awt.dnd.DropTargetDropEvent;
+import java.awt.dnd.DropTargetEvent;
+import java.awt.dnd.DropTargetListener;
+import java.io.IOException;
+import java.util.ArrayList;
 import javax.swing.DefaultListModel;
+
 import javax.swing.JList;
 import javax.swing.ListSelectionModel;
 
 /**
  * Reorderable JList.
  */
-public final class ReorderableJList extends JList {
+public final class ReorderableJList extends JList implements DragSourceListener,
+        DropTargetListener, DragGestureListener {
     
     /**
      * A version number for this class. It should be changed whenever the class
@@ -39,20 +64,193 @@ public final class ReorderableJList extends JList {
      */
     private static final long serialVersionUID = 1;
     
+    /** Drag source. */
+    private final DragSource dragSource;
+    /** Drag target. */
+    private final DropTarget dropTarget;
+    /** Drop target. */
+    private Object dropTargetCell;
+    /** Dragged index. */
+    private int draggedIndex = -1;
+    /** Data flavor. */
+    private DataFlavor dataFlavor;
+    
+    /** Instantiate new ReorderableJList. */
     public ReorderableJList() {
         super(new DefaultListModel());
         
+        setCellRenderer(new ReorderableJListCellRenderer(this));
         setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         setTransferHandler(new ArrayListTransferHandler());
-        setDragEnabled(true);
+        
+        dragSource = DragSource.getDefaultDragSource();
+        dropTarget = new DropTarget(this, this);
+        
+        dragSource.createDefaultDragGestureRecognizer(this,
+                DnDConstants.ACTION_MOVE, this);
+        try {
+            
+            dataFlavor = new DataFlavor(DataFlavor.javaJVMLocalObjectMimeType
+                    + ";class=java.util.ArrayList");
+        } catch (ClassNotFoundException e) {
+            Logger.userError(ErrorLevel.LOW, "unable to create data flavor: "
+                    + e.getMessage());
+            dataFlavor = null;
+        }
     }
     
+    /** {@inheritDoc} */
     public DefaultListModel getModel() {
         return (DefaultListModel) super.getModel();
     }
     
+    /** {@inheritDoc} */
     public void setModel(final DefaultListModel newModel) { //NOPMD stupid
         super.setModel(newModel);
+    }
+    
+    /**
+     * Returns the target drop item.
+     *
+     * @return Drop target cell
+     */
+    public Object getTargetCell() {
+        return dropTargetCell;
+    }
+    
+    /** {@inheritDoc} */
+    public void dragEnter(final DragSourceDragEvent dsde) {
+        //Ignore
+    }
+    
+    /** {@inheritDoc} */
+    public void dragOver(final DragSourceDragEvent dsde) {
+        //Ignore
+    }
+    
+    /** {@inheritDoc} */
+    public void dropActionChanged(final DragSourceDragEvent dsde) {
+        //Ignore
+    }
+    
+    /** {@inheritDoc} */
+    public void dragExit(final DragSourceEvent dse) {
+        //Ignore
+    }
+    
+    /** {@inheritDoc} */
+    public void dragDropEnd(final DragSourceDropEvent dsde) {
+        //clear drop variables and repaint
+        dropTargetCell = null;
+        draggedIndex = -1;
+        repaint();
+    }
+    
+    /** {@inheritDoc} */
+    public void dragEnter(final DropTargetDragEvent dtde) {
+        //check whether to accept drag
+        if (dtde.getSource() == dropTarget) {
+            dtde.acceptDrag(DnDConstants.ACTION_MOVE);
+        } else {
+            dtde.rejectDrag();
+        }
+    }
+    
+    /** {@inheritDoc} */
+    public void dragOver(final DropTargetDragEvent dtde) {
+        //Reject drops on self
+        if (dtde.getSource() != dropTarget) {
+            dtde.rejectDrag();
+        }
+        
+        //get location and index
+        final Point dragPoint = dtde.getLocation();
+        final int index = locationToIndex(dragPoint);
+        
+        //set drag variables and repaint
+        if (index == -1) {
+            dropTargetCell = null;
+        } else {
+            dropTargetCell = getModel().getElementAt(index);
+        }
+        
+        repaint();
+    }
+    
+    /** {@inheritDoc} */
+    public void dropActionChanged(final DropTargetDragEvent dtde) {
+        //Ignore
+    }
+    
+    /** {@inheritDoc} */
+    public void dragExit(final DropTargetEvent dte) {
+        //Ignore
+    }
+    
+    /** {@inheritDoc} */
+    public void drop(final DropTargetDropEvent dtde) {
+        //check source and reject
+        if (dtde.getSource() != dropTarget) {
+            dtde.rejectDrop();
+            return;
+        }
+        //get object location and index
+        final Point dropPoint = dtde.getLocation();
+        final int index = locationToIndex(dropPoint);
+        
+        //reject invalid drops
+        if ((index == -1) || (index == draggedIndex)) {
+            dtde.rejectDrop();
+            return;
+        }
+        
+        //accept drop as a move
+        dtde.acceptDrop(DnDConstants.ACTION_MOVE);
+        
+        //get dropped item
+        Object dragged;
+        try {
+            dragged = dtde.getTransferable().getTransferData(dataFlavor);
+        } catch (UnsupportedFlavorException e) {
+            return;
+        } catch (IOException e) {
+            return;
+        }
+        
+        //move items
+        final boolean sourceBeforeTarget = (draggedIndex < index);
+        final DefaultListModel mod = getModel();
+        final int newIndex = (sourceBeforeTarget ? index - 1 : index);
+        mod.remove(draggedIndex);
+        for (Object item : (ArrayList) dragged) {
+            mod.add(newIndex, item);
+        }
+        
+        getSelectionModel().setSelectionInterval(newIndex, newIndex);
+        
+        //drop complete
+        dtde.dropComplete(true);
+    }
+    
+    /** {@inheritDoc} */
+    public void dragGestureRecognized(final DragGestureEvent dge) {
+        //find the objects location and index
+        final Point clickPoint = dge.getDragOrigin();
+        final int index = locationToIndex(clickPoint);
+        
+        if (index == -1) {
+            return;
+        }
+        
+        //get the list object
+        final Object target = getModel().getElementAt(index);
+        //create the trasnferable object
+        final ArrayList<Object> transferObject = new ArrayList<Object>();
+        transferObject.add(target);
+        final Transferable trans = new ArrayListTransferable(transferObject);
+        //start drag
+        draggedIndex = index;
+        dragSource.startDrag(dge,Cursor.getDefaultCursor(), trans, this);
     }
 }
 
