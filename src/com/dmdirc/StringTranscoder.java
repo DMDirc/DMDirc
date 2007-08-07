2be94328e07ca53429a5b0d51aa0dfbfc7dbbae5
Chris Smith <chris@dmdirc.com>
2007-08-07 14:35:55 +0000

    Added StringTranscoder
    git-svn-id: http://svn.dmdirc.com/trunk@1811 00569f92-eb28-0410-84fd-f71c24880f43

---
+++ src/com/dmdirc/StringTranscoder.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2006-2007 Chris Smith, Shane Mc Cormack, Gregory Holmes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc;
+
+import java.nio.charset.Charset;
+
+/**
+ * Facilitates easy transcoding of strings.
+ * 
+ * @author Chris
+ */
+public class StringTranscoder {
+   
+    private final Charset charset;
+    
+    /**
+     * Creates a new StringTranscoder that will transcode strings from the
+     * system's default character set into the specified charset.
+     * 
+     * @param charset The target character set
+     */
+    public StringTranscoder(final Charset charset) {
+        this.charset = charset;
+    }
+    
+    /**
+     * Transcodes the specified string from the system's default charset to
+     * this transcoder's charset.
+     * 
+     * @param string The string to be transcoded
+     * @return A new string of the appropriate charset
+     */
+    public String encode(final String string) {
+        return new String(string.getBytes(charset));
+    }
+    
+    /**
+     * Transcodes the specified string from this transcoder's charset to the
+     * system's default charset
+     * 
+     * @param string The string to be transcoded
+     * @return A new string of the appropriate charset
+     */
+    public String decode(final String string) {
+        return new String(string.getBytes(), charset);
+    }    
+    
+}
---
+++ test/com/dmdirc/StringTranscoderTest.java
@@ -0,0 +1,63 @@
+/*
+ * StringTranscoderTest.java
+ * JUnit 4.x based test
+ *
+ * Created on 07 August 2007, 15:10
+ */
+
+package com.dmdirc;
+
+import java.nio.charset.Charset;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Chris
+ */
+public class StringTranscoderTest {
+    
+    public StringTranscoderTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void transcode() {
+        System.out.println("transcode");
+        String string = new String(new byte[]{(byte) 0xCA, (byte) 0xAE});
+        System.out.println(string);
+        
+        StringTranscoder instance = new StringTranscoder(Charset.forName("UTF-8"));
+        
+        String res = instance.encode(string);
+        System.out.println(res);
+        byte[] result = res.getBytes();
+        
+        for (byte part : result) {
+            System.out.print(" " + part);
+        }
+        System.out.println();
+        
+        assertEquals(string, instance.decode(new String(result)));
+    }
+    
+}
