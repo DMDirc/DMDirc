e82cd05d4e8cd62a9fd8b558d81eb88f2c06591d
Chris Smith <chris@dmdirc.com>
2007-09-03 12:16:00 +0000

    Initial work on an improved actions substitutor
    git-svn-id: http://svn.dmdirc.com/trunk@1919 00569f92-eb28-0410-84fd-f71c24880f43

---
+++ src/com/dmdirc/actions/ActionSubstitutor.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2006-2007 Chris Smith, Shane Mc Cormack, Gregory Holmes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.actions;
+
+import com.dmdirc.Config;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Handles the substitution of variables into action targets and responses.
+ *
+ * @author Chris
+ */
+public class ActionSubstitutor {
+    
+    /** The action type this substitutor is for. */
+    private final ActionType type;
+    
+    /**
+     * Creates a new substitutor for the specified action type.
+     *
+     * @param type The action type this substitutor is for
+     */
+    public ActionSubstitutor(final ActionType type) {
+        this.type = type;
+    }
+    
+    /**
+     * Retrieves a list of global config variables that will be substituted.
+     * Note: does not include initial $.
+     *
+     * @return A list of global variable names that will be substituted
+     */
+    public List<String> getConfigSubstitutions() {
+        return Config.getOptions("actions");
+    }
+    
+    /**
+     * Retrieves a list of substitutions derived from argument and component
+     * combinations, along with a corresponding friendly name for them.
+     * Note: does not include initial $.
+     *
+     * @return A map of component substitution names and their descriptions
+     */
+    public Map<String, String> getComponentSubstitutions() {
+        final Map<String, String> res = new HashMap<String, String>();
+        
+        int i = 0;
+        for (Class myClass : type.getType().getArgTypes()) {
+            for (ActionComponent comp : ActionManager.getCompatibleComponents(myClass)) {
+                final String key = "{" + i + "." + comp.toString() + "}";
+                final String desc = type.getType().getArgNames()[i] + "'s " + comp.getName();
+                
+                res.put(key, desc);
+            }
+            
+            i++;
+        }
+        
+        return res;
+    }
+    
+}
