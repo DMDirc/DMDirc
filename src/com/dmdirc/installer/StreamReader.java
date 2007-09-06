d163917a162761ded1dbd4909f620ffcea0a7ffe
Shane Mc Cormack <shane@dmdirc.com>
2007-09-06 04:16:22 +0000

    Added stuff that was needed by the last commit
    git-svn-id: http://svn.dmdirc.com/trunk@1989 00569f92-eb28-0410-84fd-f71c24880f43

---
+++ src/com/dmdirc/installer/StreamReader.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2006-2007 Chris Smith, Shane Mc Cormack, Gregory Holmes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.installer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class StreamReader extends Thread {
+	/** This is the Input Stream we are reading */
+	private InputStream stream;
+	
+	/**
+	 * Create a new Stream Reader
+	 *
+	 * @param stream The stream to read
+	 */
+	public StreamReader(final InputStream stream) {
+		this.stream = stream;
+	}
+
+	public void run() {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+		try {
+			while (reader.readLine() != null) {
+				/* Do Nothing */
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				stream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
