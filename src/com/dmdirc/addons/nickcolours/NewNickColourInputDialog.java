acf0b211fb34561cc9ac08104361d9bacb895fd3
Gregory Holmes <greg@dmdirc.com>
2007-09-10 22:52:51 +0000

    Some kind of basic nick colour add thing for md
    git-svn-id: http://svn.dmdirc.com/trunk@2018 00569f92-eb28-0410-84fd-f71c24880f43

---
+++ src/com/dmdirc/addons/nickcolours/NewNickColourInputDialog.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright (c) 2006-2007 Chris Smith, Shane Mc Cormack, Gregory Holmes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.addons.nickcolours;
+
+import com.dmdirc.Main;
+import com.dmdirc.ui.swing.MainFrame;
+import com.dmdirc.ui.swing.components.ColourPickerPanel;
+import com.dmdirc.ui.swing.components.StandardDialog;
+import static com.dmdirc.ui.swing.UIUtilities.layoutGrid;
+import static com.dmdirc.ui.swing.UIUtilities.SMALL_BORDER;
+import java.awt.BorderLayout;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+
+/**
+ * New nick colour input dialog.
+ */
+public class NewNickColourInputDialog extends StandardDialog 
+        implements ActionListener {
+    
+    /** Buttons panel. */
+    private JPanel buttonsPanel;
+    /** content panel. */
+    private JPanel contentPanel;
+    
+    /** nickname textfield. */
+    private JTextField nickname;
+    /** network textfield. */
+    private JTextField network;
+    /** text colour input. */
+    private ColourPickerPanel textColour;
+    /** nicklist colour input. */
+    private ColourPickerPanel nicklistColour;
+    
+    /** Creates a new instance of NewNickColourInputDialog. */
+    public NewNickColourInputDialog() {
+        //super((MainFrame) Main.getUI().getMainWindow(), false);
+        super(null, false);
+        
+        initComponents();
+        initListeners();
+        layoutComponents();
+    }
+    
+    /** Initialises the components. */
+    private void initComponents() {
+        contentPanel = new JPanel();
+        buttonsPanel = new JPanel();
+        
+        orderButtons(new JButton(), new JButton());
+        
+        nickname = new JTextField();
+        network = new JTextField();
+        textColour = new ColourPickerPanel(true, true);
+        nicklistColour = new ColourPickerPanel(true, true);
+    }
+    
+    /** Initialises the content panel. */
+    private void layoutContentPanel() {        
+        contentPanel.setLayout(new SpringLayout());
+        
+        contentPanel.add(new JLabel("Nickname: "));
+        contentPanel.add(nickname);
+        
+        contentPanel.add(new JLabel("Network: "));
+        contentPanel.add(network);
+        
+        contentPanel.add(new JLabel("Text colour: "));
+        contentPanel.add(textColour);
+        
+        contentPanel.add(new JLabel("Nicklist colour: "));
+        contentPanel.add(nicklistColour);
+        
+        layoutGrid(contentPanel, 4, 2, SMALL_BORDER, SMALL_BORDER, 
+                SMALL_BORDER, SMALL_BORDER);
+    }
+    
+    /** Initialises the button panel. */
+    private void layoutButtonsPanel() {        
+        buttonsPanel.setBorder(BorderFactory.createEmptyBorder(SMALL_BORDER,
+                SMALL_BORDER, SMALL_BORDER, SMALL_BORDER));
+        
+        buttonsPanel.setLayout(new BoxLayout(buttonsPanel, BoxLayout.LINE_AXIS));
+        buttonsPanel.add(Box.createHorizontalGlue());
+        buttonsPanel.add(getLeftButton());
+        buttonsPanel.add(Box.createHorizontalStrut(SMALL_BORDER));
+        buttonsPanel.add(getRightButton());
+    }
+    
+    /** Initialises the listeners. */
+    private void initListeners() {
+        textColour.addActionListener(this);
+        nicklistColour.addActionListener(this);
+    }
+    
+    /** Lays out the components. */
+    private void layoutComponents() {
+        layoutContentPanel();
+        layoutButtonsPanel();
+
+        setLayout(new BorderLayout());
+        
+        add(contentPanel, BorderLayout.CENTER);
+        add(buttonsPanel, BorderLayout.PAGE_END);
+        
+        pack();
+    }
+
+    /** {@inheritDoc} */
+    public void actionPerformed(ActionEvent e) {
+    }
+    
+    public static void main(final String[] args) {
+        new NewNickColourInputDialog().setVisible(true);
+    }
+    
+}
