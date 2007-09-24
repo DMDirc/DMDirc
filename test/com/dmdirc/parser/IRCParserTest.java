d26579cdc80afff21cdcd807708c2690a33d02e4
Chris Smith <chris@dmdirc.com>
2007-09-24 23:05:47 +0000

    Add unit test for the parser's tokeniser
    git-svn-id: http://svn.dmdirc.com/trunk@2144 00569f92-eb28-0410-84fd-f71c24880f43

--- test/com/dmdirc/parser/IRCParserTest.java
+++ test/com/dmdirc/parser/IRCParserTest.java
@@ -39,4 +39,29 @@ public class IRCParserTest extends TestCase {
         assertTrue("addCallback() should throw exception for non-existant callbacks", res);
     }
     
+    @Test
+    public void testTokeniser() {
+        final IRCParser myParser = new IRCParser();
+        
+        final String line1 = "a b c d e";
+        final String line2 = "a b c :d e";
+        final String line3 = ":a b:c :d e";
+        
+        final String[] res1 = myParser.tokeniseLine(line1);
+        final String[] res2 = myParser.tokeniseLine(line2);
+        final String[] res3 = myParser.tokeniseLine(line3);
+        
+        arrayEquals(res1, new String[]{"a", "b", "c", "d", "e"});
+        arrayEquals(res2, new String[]{"a", "b", "c", "d e"});
+        arrayEquals(res3, new String[]{":a", "b:c", "d e"});
+    }
+    
+    private void arrayEquals(final String[] a1, final String[] a2) {
+        assertEquals(a1.length, a2.length);
+        
+        for (int i = 0; i < a1.length; i++) {
+            assertEquals(a1[i], a2[i]);
+        }
+    }
+    
 }
