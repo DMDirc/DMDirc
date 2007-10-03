7b7acdf77d939dd918f28a75f60ec97913428208
Chris Smith <chris@dmdirc.com>
2007-10-03 17:38:08 +0000

    Add unit test to ensure the global config only has global identities
    git-svn-id: http://svn.dmdirc.com/trunk@2363 00569f92-eb28-0410-84fd-f71c24880f43

---
+++ test/com/dmdirc/config/IdentityManagerTest.java
@@ -0,0 +1,38 @@
+/*
+ * IdentityManagerTest.java
+ * JUnit based test
+ *
+ * Created on 03 October 2007, 18:34
+ */
+
+package com.dmdirc.config;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author chris
+ */
+public class IdentityManagerTest extends TestCase {
+    
+    public IdentityManagerTest(String testName) {
+        super(testName);
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        
+        IdentityManager.load();
+    }
+
+    public void testGetGlobalConfig() {
+        final ConfigManager gcm = IdentityManager.getGlobalConfig();
+        
+        for (Identity identity : gcm.getSources()) {
+            assertTrue(identity.getTarget().getType() == ConfigTarget.TYPE_GLOBAL
+                    || identity.getTarget().getType() == ConfigTarget.TYPE_GLOBALDEFAULT
+                    || identity.getTarget().getType() == ConfigTarget.TYPE_THEME);
+        }
+    }
+    
+}
