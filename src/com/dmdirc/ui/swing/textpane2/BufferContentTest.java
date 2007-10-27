769e25dfa3d4e72297f6511b173e7ec0c57f6ba3
Gregory Holmes <greg@dmdirc.com>
2007-10-27 00:42:48 +0000

    test app for new textpane model
    git-svn-id: http://svn.dmdirc.com/trunk@2565 00569f92-eb28-0410-84fd-f71c24880f43

---
+++ src/com/dmdirc/ui/swing/textpane2/BufferContentTest.java
@@ -0,0 +1,45 @@
+package com.dmdirc.ui.swing.textpane2;
+
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JTextPane;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.StyleContext;
+
+public class BufferContentTest {
+
+    public static void main(String[] args) throws BadLocationException {
+
+        final JFrame frame = new JFrame();
+        final JTextPane tp = new JTextPane();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(800, 600));
+        tp.setEditable(false);
+        tp.setDocument(new DefaultStyledDocument(new BufferContent(), new StyleContext()));
+
+        frame.add(tp);
+
+        frame.pack();
+
+        frame.setVisible(true);
+        
+        tp.getDocument().insertString(tp.getDocument().getLength(), "RAR\n", null);
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ex) {
+            //Ignore
+        }
+        
+        tp.getDocument().insertString(tp.getDocument().getLength(), "RAR1\n", null);
+        
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ex) {
+            //Ignore
+        }
+        
+        tp.getDocument().insertString(tp.getDocument().getLength(), "RAR2\n", null);
+    }
+}
