86bc245e80b797fbf16814eefa6146453b984298
Gregory Holmes <greg@dmdirc.com>
2007-11-02 15:42:01 +0000

    createAllPluginJar.sh is now run in the ant build script (i'd like do this this in ant, but cant seem to find a way to list directories in ant)
    git-svn-id: http://svn.dmdirc.com/trunk@2623 00569f92-eb28-0410-84fd-f71c24880f43

--- createAllPluginJar.sh
+++ createAllPluginJar.sh
@@ -2,7 +2,6 @@
 # This attempts to create a clean build of dmdirc, then if the build was a success
 # it will create plugin jars for all the plugins in the addons directory.
 
-ant clean jar
 if [ ${?} = "0" ]; then
 	for dir in `ls -1 src/com/dmdirc/addons`; do
 		./createPluginJar.sh com.dmdirc.addons.${dir} ${dir}
