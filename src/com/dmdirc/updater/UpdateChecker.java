b3b4e99632394039343508540f4b6dce5423834d
Chris Smith <chris@dmdirc.com>
2007-11-05 15:21:59 +0000

    Fixed update checker's update listener being GC'd Removed status bar message
    git-svn-id: http://svn.dmdirc.com/trunk@2647 00569f92-eb28-0410-84fd-f71c24880f43

--- src/com/dmdirc/updater/UpdateChecker.java
+++ src/com/dmdirc/updater/UpdateChecker.java
@@ -51,8 +51,8 @@ import java.util.TimerTask;
  *
  * @author chris
  */
-public final class UpdateChecker implements Runnable {
-    
+public final class UpdateChecker implements Runnable, UpdateListener {
+
     /** The possible states for the checker. */
     public static enum STATE {
         /** Nothing's happening. */
@@ -62,56 +62,54 @@ public final class UpdateChecker implements Runnable {
         /** New updates are available. */
         UPDATES_AVAILABLE
     }
-       
+
     /** A list of components that we're to check. */
     private static final List<UpdateComponent> components
             = new ArrayList<UpdateComponent>();
-        
+
     /** Our timer. */
     private static Timer timer = new Timer("Update Checker Timer");
-    
+
     /** The list of updates that are available. */
     private static final List<Update> updates = new ArrayList<Update>();
-    
+
     /** A list of our listeners. */
     private static final ListenerList listeners = new ListenerList();
-    
+
     /** Our current state. */
     private static STATE status = STATE.IDLE;
-    
+
     static {
         components.add(new ClientComponent());
         components.add(new ModeAliasesComponent());
         components.add(new DefaultsComponent());
-    }    
-    
+    }
+
     /**
      * Instantiates an Updatechecker.
      */
     public UpdateChecker() {
         //Ignore
     }
-    
+
     /** {@inheritDoc} */
     @Override
     public void run() {
         final ConfigManager config = IdentityManager.getGlobalConfig();
-        
+
         if (!config.getOptionBool("updater", "enable", true)) {
             init();
             return;
         }
-        
+
         setStatus(STATE.CHECKING);
-        
-        Main.getUI().getStatusBar().setMessage("Checking for updates...");
-        
+
         updates.clear();
-        
+
         final StringBuilder data = new StringBuilder();
-        final String updateChannel 
+        final String updateChannel
                 = config.getOption("updater", "channel", Main.UPDATE_CHANNEL.toString());
-        
+
         for (UpdateComponent component : components) {
             if (config.getOptionBool("updater", "enable-" + component.getName(), true)) {
                 data.append(component.getName());
@@ -122,37 +120,37 @@ public final class UpdateChecker implements Runnable {
                 data.append(';');
             }
         }
-        
+
         if (data.length() > 0) {
-            try {            
+            try {
                 final List<String> response
                     = Downloader.getPage("http://updates.dmdirc.com/", "data=" + data);
-                
+
                 updates.clear();
-            
+
                 for (String line : response) {
                     checkLine(line);
                 }
             } catch (MalformedURLException ex) {
                 Logger.appError(ErrorLevel.LOW, "Error when checking for updates", ex);
             } catch (IOException ex) {
-                Logger.userError(ErrorLevel.LOW, 
+                Logger.userError(ErrorLevel.LOW,
                         "I/O error when checking for updates: " + ex.getMessage());
             }
         }
-        
+
         if (updates.isEmpty()) {
             setStatus(STATE.IDLE);
         } else {
             setStatus(STATE.UPDATES_AVAILABLE);
         }
-        
+
         UpdateChecker.init();
-        
+
         IdentityManager.getConfigIdentity().setOption("updater",
-                "lastcheck", String.valueOf((int) (new Date().getTime() / 1000)));        
+                "lastcheck", String.valueOf((int) (new Date().getTime() / 1000)));
     }
-    
+
     /**
      * Checks the specified line to determine the message from the update server.
      *
@@ -171,7 +169,7 @@ public final class UpdateChecker implements Runnable {
                     + line);
         }
     }
-    
+
     /**
      * Informs the user that there's an update available.
      *
@@ -179,20 +177,20 @@ public final class UpdateChecker implements Runnable {
      */
     public void doUpdateAvailable(final String line) {
         final Update update = new Update(line);
-        
+
         updates.add(update);
-        update.addUpdateListener(new UpdateListener() {
-            /** {@inheritDoc} */
-            @Override
-            public void updateStatusChange(Update update, STATUS status) {
-                if (status == Update.STATUS.INSTALLED
-                        || status == Update.STATUS.ERROR) {
-                    removeUpdate(update);
-                }
-            }
-        });
+        update.addUpdateListener(this);
     }
-    
+
+    /** {@inheritDoc} */
+    @Override
+    public void updateStatusChange(final Update update, final STATUS status) {
+        if (status == Update.STATUS.INSTALLED
+                || status == Update.STATUS.ERROR) {
+            removeUpdate(update);
+        }
+    }
+
     /**
      * Initialises the update checker. Sets a timer to check based on the
      * frequency specified in the config.
@@ -204,18 +202,18 @@ public final class UpdateChecker implements Runnable {
                 = IdentityManager.getGlobalConfig().getOptionInt("updater", "frequency", 86400);
         final int timestamp = (int) (new Date().getTime() / 1000);
         int time = 0;
-        
+
         if (last + freq > timestamp) {
             time = last + freq - timestamp;
         }
-        
+
         if (time > freq || time < 0) {
             Logger.userError(ErrorLevel.LOW, "Attempted to schedule update check "
                     + (time < 0 ? "in the past" : "too far in the future")
                     + ", rescheduling.");
             time = 1;
         }
-             
+
         timer.cancel();
         timer = new Timer("Update Checker Timer");
         timer.schedule(new TimerTask() {
@@ -226,51 +224,51 @@ public final class UpdateChecker implements Runnable {
             }
         }, time * 1000);
     }
-    
+
     /**
      * Checks for updates now.
      */
     public static void checkNow() {
         new Thread(new UpdateChecker(), "Update Checker thread").start();
     }
-    
+
     /**
      * Registers an update component.
-     * 
+     *
      * @param component The component to be registered
      */
     public static void registerComponent(final UpdateComponent component) {
         components.add(component);
     }
-    
+
     /**
      * Finds and returns the component with the specified name.
-     * 
+     *
      * @param name The name of the component that we're looking for
      * @return The corresponding UpdateComponent, or null if it's not found
      */
     @Precondition("The specified name is not null")
     public static UpdateComponent findComponent(final String name) {
         assert(name != null);
-        
+
         for (UpdateComponent component : components) {
             if (name.equals(component.getName())) {
                 return component;
             }
         }
-       
+
         return null;
     }
-    
+
     /**
      * Removes the specified update from the list. This should be called when
      * the update has finished or has encountered an error.
-     * 
+     *
      * @param update The update to be removed
      */
     private static void removeUpdate(final Update update) {
         updates.remove(update);
-        
+
         if (updates.isEmpty()) {
             setStatus(STATE.IDLE);
         }
@@ -278,61 +276,61 @@ public final class UpdateChecker implements Runnable {
 
     /**
      * Retrieves a list of components registered with the checker.
-     * 
+     *
      * @return A list of registered components
      */
     public static List<UpdateComponent> getComponents() {
         return components;
     }
-    
+
     /**
      * Retrives a list of available updates from the checker.
-     * 
+     *
      * @return A list of available updates
      */
     public static List<Update> getAvailableUpdates() {
         return updates;
     }
-    
-    
+
+
     /**
      * Adds a new status listener to the update checker.
-     * 
+     *
      * @param listener The listener to be added
      */
     public static void addListener(final UpdateCheckerListener listener) {
         listeners.add(UpdateCheckerListener.class, listener);
     }
-    
+
     /**
      * Removes a status listener from the update checker.
-     * 
+     *
      * @param listener The listener to be removed
      */
     public static void removeListener(final UpdateCheckerListener listener) {
         listeners.remove(UpdateCheckerListener.class, listener);
     }
-    
+
     /**
      * Retrieves the current status of the update checker.
-     * 
+     *
      * @return The update checker's current status
      */
     public static STATE getStatus() {
         return status;
     }
-    
+
     /**
      * Sets the status of the update checker to the specified new status.
-     * 
+     *
      * @param newStatus The new status of this checker
      */
     private static void setStatus(final STATE newStatus) {
         status = newStatus;
-        
+
         for (UpdateCheckerListener listener : listeners.get(UpdateCheckerListener.class)) {
             listener.statusChanged(newStatus);
         }
     }
-    
+
 }
