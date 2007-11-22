03795feb0f1aeed2091b74fa4e4e8dd4c508774b
Shane Mc Cormack <shane@dmdirc.com>
2007-11-22 17:58:29 +0000

    JWrappingLabel:  => Now wraps on "\n"  => Overrides getPreferredSize()      => if setPreferredSize() is set with a non-null value, that value is always returned      => if getWidth() > 0 then Preferred Size is determined with getWidth() as the maximum width, else no maximum width is assumed.
    git-svn-id: http://svn.dmdirc.com/trunk@2738 00569f92-eb28-0410-84fd-f71c24880f43

--- src/com/dmdirc/ui/swing/JWrappingLabel.java
+++ src/com/dmdirc/ui/swing/JWrappingLabel.java
@@ -23,6 +23,7 @@
 package com.dmdirc.ui.swing;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Rectangle2D;
@@ -38,6 +39,19 @@ import javax.swing.SwingConstants;
  * @author Shane Mc Cormack
  */
 public class JWrappingLabel extends JLabel {
+	/**
+	 * A version number for this class.
+	 * It should be changed whenever the class structure is changed (or anything
+	 * else that would prevent serialized objects being unserialized with the
+	 * new class).
+	 */
+	private static final long serialVersionUID = 2007112201;
+
+	/** My prefered height. */
+	private int myPreferredHeight = 0;
+	/** My prefered width. */
+	private int myPreferredWidth = 0;
+
 	/** {@inheritDoc} */
 	public JWrappingLabel() {
 		super();
@@ -54,102 +68,159 @@ public class JWrappingLabel extends JLabel {
 	}
 	
 	/** {@inheritDoc} */
+	public void setText(final String text) {
+		super.setText(text);
+		myPreferredHeight = 0;
+		myPreferredWidth = 0;
+	}
+	
+	/** {@inheritDoc} */
+	public Dimension getPreferredSize() {
+		if (isPreferredSizeSet()) {
+			return super.getPreferredSize();
+		} else {
+			doPaint(getGraphics(), true);
+			return new Dimension(myPreferredWidth, myPreferredHeight);
+		}
+	}
+	
+	/** {@inheritDoc} */
 	@Override
 	public void paint(final Graphics g) {
+		doPaint(g, false);
+	}
+	
+	/**
+	 * This method handles painting.
+	 *
+	 * @param g Graphics object to paint into
+	 * @param getPreferred If true, assume unlimited width and height, and don't
+	 *                     actually paint, only set the values for
+	 *                     myPreferredHeight and myPreferredWidth
+	 */
+	private void doPaint(final Graphics g, final boolean getPreferred) {
 		// Get a decent graphics object
 		final Graphics2D g2 = (Graphics2D)g;
 		
-		if (isOpaque()) {
+		if (!getPreferred && isOpaque()) {
 			Color oldColor = g.getColor();
 			g.setColor(getBackground());
 			g.fillRect(0, 0, getWidth(), getHeight());
 			g.setColor(oldColor);
 		}
 		
-		// Split the text
-		final String[] bits = getText().split(" ");
-		
-		// This is used to make sure we don't draw a blank line at the top if the
-		// first word is too long.
-		boolean isFirst = true;
+		// Split the lines
+		final String[] allLines = getText().split("\n");
 		
 		// Current drawing positions
 		int top = 0;
 		int height = 0;
-		int left = 0;
-		
+		int maxLeft = 0;
 		// The width of a space
 		int spaceWidth = (int)g.getFont().getStringBounds(" ", g2.getFontRenderContext()).getWidth();
 		
-		// Store each line to draw afterwards (allows for alignment)
-		ArrayList<String> lines = new ArrayList<String>();
-		// The line we have atm
-		StringBuilder line = new StringBuilder();
-		
-		// Now loop through the words
-		for (String bit : bits) {
-			// Get the sizes of this word
-			Rectangle2D bounds = g.getFont().getStringBounds(bit, g2.getFontRenderContext());
-			LineMetrics metrics = g.getFont().getLineMetrics(bit, g2.getFontRenderContext());
+		for (String thisLine : allLines) {
+			int left = 0;
+			// Top of this block of lines
+			int startTop = top;
+			// Split the text
+			final String[] bits = thisLine.split(" ");
 			
-			// Width of this word
-			int thisWidth = (int)bounds.getWidth();
+			// This is used to make sure we don't draw a blank line at the top if the
+			// first word is too long.
+			boolean isFirst = true;
 			
-			// Update line height so we know how far to drop down when drawing the
-			// next line
-			if (height < bounds.getHeight()) {
-				height = (int)bounds.getHeight();
-			}
+			// Store each line to draw afterwards (allows for alignment)
+			ArrayList<String> lines = new ArrayList<String>();
+			// The line we have atm
+			StringBuilder line = new StringBuilder();
 			
-			// Check if this word fits on the current line or not.
-			// If there are no other words on this line, we assume the word fits
-			// (Thus long words don't get split up)
-			if (thisWidth+left > getWidth() && thisWidth != 0) {
-				// It doesn't fit so we need to move down a line.
+			// Now loop through the words
+			for (String bit : bits) {
+				// Get the sizes of this word
+				Rectangle2D bounds = g.getFont().getStringBounds(bit, g2.getFontRenderContext());
+				LineMetrics metrics = g.getFont().getLineMetrics(bit, g2.getFontRenderContext());
 				
-				// This stops us leaving a blank line at the very top if the first
-				// word is too big.
-				if (!isFirst) {
-					top = top + height;
-					height = 0;
-					lines.add(line.toString());
-					line = new StringBuilder();
+				// Width of this word
+				int thisWidth = (int)bounds.getWidth();
+				
+				// Update line height so we know how far to drop down when drawing the
+				// next line
+				if (height < bounds.getHeight()) {
+					height = (int)bounds.getHeight();
 				}
-				left = 0;
+				
+				
+				boolean needNewLine;
+				// Check if this word fits on the current line or not.
+				// If there are no other words on this line, we assume the word fits
+				// (Thus long words don't get split up)
+				if ((isPreferredSizeSet() || myPreferredWidth > 0) && getWidth() > 0) {
+					needNewLine = (thisWidth+left > getWidth() && thisWidth != 0);
+				} else {
+					// If no preferred width is set, we assume we can draw as wide as we
+					// want!
+					needNewLine = false;
+				}
+				if (needNewLine) {
+					// It doesn't fit so we need to move down a line.
+					
+					// This stops us leaving a blank line at the very top if the first
+					// word is too big.
+					if (!isFirst) {
+						top = top + height;
+						height = 0;
+						lines.add(line.toString());
+						line = new StringBuilder();
+					}
+					left = 0;
+				}
+				left = left+thisWidth+spaceWidth;
+				if (maxLeft < left) { maxLeft = left; }
+				line.append(bit+" ");
+				// And thats that!
+				isFirst = false;
 			}
-			left = left+thisWidth+spaceWidth;
-			line.append(bit+" ");
-			// And thats that!
-			isFirst = false;
-		}
-		if (line.length() > 0) { lines.add(line.toString()); }
-		
-		// Now draw
-		top = 0;
-		for (String drawLine : lines) {
-			Rectangle2D bounds = g.getFont().getStringBounds(drawLine, g2.getFontRenderContext());
-			LineMetrics metrics = g.getFont().getLineMetrics(drawLine, g2.getFontRenderContext());
+			if (line.length() > 0) { lines.add(line.toString()); }
 			
-			// However, we need to take into account the overhang in characters like y and g
-			int y = top + (int)Math.round(bounds.getHeight() - metrics.getDescent());
-			
-			// Now to get where the left should go.
-			int x;
-			int alignment = getHorizontalAlignment();
-			if (alignment == SwingConstants.CENTER) {
-				x = (int)((getWidth()/2) - (bounds.getWidth()/2));
-			} else if (alignment == SwingConstants.RIGHT) {
-				x = (int)(getWidth() - bounds.getWidth());
-			} else {
-				x = 0;
+			// Now draw
+			top = startTop;
+			for (String drawLine : lines) {
+				Rectangle2D bounds = g.getFont().getStringBounds(drawLine, g2.getFontRenderContext());
+				
+				// Only draw if we are not just trying to get the preferred size.
+				// otherwise we just update top
+				if (!getPreferred) {
+					LineMetrics metrics = g.getFont().getLineMetrics(drawLine, g2.getFontRenderContext());
+				
+					// However, we need to take into account the overhang in characters like y and g
+					int y = top + (int)Math.round(bounds.getHeight() - metrics.getDescent());
+					
+					// Now to get where the left should go.
+					int x;
+					int alignment = getHorizontalAlignment();
+					if (alignment == SwingConstants.CENTER) {
+						x = (int)((getWidth()/2) - (bounds.getWidth()/2));
+					} else if (alignment == SwingConstants.RIGHT) {
+						x = (int)(getWidth() - bounds.getWidth());
+					} else {
+						x = 0;
+					}
+				
+					// And finally actually draw the word
+					g.drawString(drawLine, x, y);
+				}
+				top = (int)(top + bounds.getHeight());
 			}
-			
-			// And finally actually draw the word
-			g.drawString(drawLine, x, y);
-			top = (int)(top + bounds.getHeight());
 		}
 		
-		// Update component height.
-		setSize(getWidth(), top);
+		if (!getPreferred) {
+			// Update component height.
+			setSize(getWidth(), top);
+		} else {
+			// Update prefered sizes
+			myPreferredHeight = top;
+			myPreferredWidth = maxLeft;
+		}
 	}
 }
