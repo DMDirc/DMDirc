63e1fdc9319987cc0051226fedb7f566860954e6
Shane Mc Cormack <shane@dmdirc.com>
2007-11-27 05:40:09 +0000

    Started dcc plugin
    git-svn-id: http://svn.dmdirc.com/trunk@2812 00569f92-eb28-0410-84fd-f71c24880f43

---
+++ src/com/dmdirc/addons/dcc/DCCFrame.java
@@ -0,0 +1,220 @@
+/*
+ * Copyright (c) 2006-2007 Chris Smith, Shane Mc Cormack, Gregory Holmes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.addons.dcc;
+
+import com.dmdirc.WritableFrameContainer;
+import com.dmdirc.ui.swing.components.InputFrame;
+import com.dmdirc.Main;
+import com.dmdirc.commandparser.CommandManager;
+import com.dmdirc.commandparser.parsers.GlobalCommandParser;
+import com.dmdirc.commandparser.parsers.CommandParser;
+import com.dmdirc.commandparser.commands.Command;
+import com.dmdirc.commandparser.commands.GlobalCommand;
+
+import com.dmdirc.IconManager;
+import com.dmdirc.Server;
+import com.dmdirc.ui.WindowManager;
+import com.dmdirc.ui.interfaces.InputWindow;
+import com.dmdirc.ui.swing.components.Frame;
+
+/**
+ * This class links DCC objects to a window.
+ *
+ * @author Shane 'Dataforce' McCormack
+ * @version $Id: DCC.java 969 2007-04-30 18:38:20Z ShaneMcC $
+ */
+public abstract class DCCFrame extends WritableFrameContainer {
+	/**
+	 * Empty Frame.
+	 */
+	private class EmptyFrame extends InputFrame {
+			/** A version number for this class. */
+			private static final long serialVersionUID = 200711271;
+			
+			/**
+			 * Creates a new instance of EmptyFrame.
+			 *
+			 * @param owner The frame container that owns this frame
+			 */
+			public EmptyFrame(final WritableFrameContainer owner) {
+					super(owner);
+					pack();
+			}
+			
+			/**
+			 * Retrieves the command Parser for this input window.
+			 *
+			 * @return This window's command parser
+			 */
+			public final CommandParser getCommandParser() {
+				return GlobalCommandParser.getGlobalCommandParser();
+			}
+	}
+	
+	/**
+	 * DCC CommandParser
+	 */
+	private static class DCCCommandParser extends CommandParser {
+		/** A version number for this class. */
+		private static final long serialVersionUID = 200711271;
+		
+		/** Loads the relevant commands into the parser. */
+		protected void loadCommands() { CommandManager.loadGlobalCommands(this); }
+		
+		/**
+		 * Executes the specified command with the given arguments.
+		 *
+		 * @param origin The window in which the command was typed
+		 * @param isSilent Whether the command is being silenced or not
+		 * @param command The command to be executed
+		 * @param args The arguments to the command
+		 */
+		protected void executeCommand(final InputWindow origin, final boolean isSilent, final Command command, final String... args) {
+			((GlobalCommand) command).execute(origin, isSilent, args);
+		}
+		
+		/**
+		 * Called when the input was a line of text that was not a command.
+		 * This normally means it is sent to the server/channel/user as-is, with
+		 * no further processing.
+		 *
+		 * @param origin The window in which the command was typed
+		 * @param line The line input by the user
+		 */
+		protected void handleNonCommand(final InputWindow origin, final String line) {
+			if (origin.getContainer() instanceof WritableFrameContainer) {
+				((WritableFrameContainer)origin.getContainer()).sendLine(line);
+			}
+		}
+	}
+	
+	/** The singleton instance of the DCC command parser. */
+	private static DCCCommandParser myDCCCommandParser;
+
+	/** The window title. */
+	protected final String title;
+	/** The Window we're using. */
+	protected InputWindow myWindow = null;
+	/** The dcc plugin that owns this frame */
+	protected final DCCPlugin plugin;
+	
+	/**
+	 * Creates a new instance of DCCFrame with an empty window.
+	 *
+	 * @param plugin The DCCPlugin that owns this frame
+	 * @param title The title of this window
+	 */
+	public DCCFrame(final DCCPlugin plugin, final String title) {
+		this(plugin, title, true);
+	}
+	
+	/**
+	 * Creates a new instance of DCCFrame.
+	 *
+	 * @param plugin The DCCPlugin that owns this frame
+	 * @param title The title of this window
+	 * @param defaultWindow Create default (empty) window.
+	 */
+	public DCCFrame(final DCCPlugin plugin, final String title, final boolean defaultWindow) {
+		super();
+		this.title = title;
+		this.plugin = plugin;
+		
+		icon = IconManager.getIconManager().getIcon("raw");
+
+		if (defaultWindow) {
+			myWindow = new EmptyFrame(this);
+			myWindow.setTitle(title);
+			myWindow.setFrameIcon(icon);
+			myWindow.setVisible(true);
+		}
+	}
+	
+	/**
+	 * Retrieves a singleton instance of the DCC command parser.
+	 *
+	 * @return A DCCCommandParser
+	 */
+	public static synchronized DCCCommandParser getDCCCommandParser() {
+		if (myDCCCommandParser == null) {
+			myDCCCommandParser = new DCCCommandParser();
+		}
+		return myDCCCommandParser;
+	}
+	
+	/**
+	 * Sends a line of text to this container's source.
+	 *
+	 * @param line The line to be sent
+	 */
+	public void sendLine(final String line) {
+		return;
+	}
+	
+	/**
+	 * Returns the maximum length that a line passed to sendLine() should be,
+	 * in order to prevent it being truncated or causing protocol violations.
+	 *
+	 * @return The maximum line length for this container
+	 */
+	public int getMaxLineLength() {
+		return 512;
+	}
+	
+	/**
+	 * Returns the internal frame associated with this object.
+	 *
+	 * @return The internal frame associated with this object
+	 */
+	public InputWindow getFrame() {
+		return myWindow;
+	}
+	
+	/**
+	 * Returns a string identifier for this object/its frame.
+	 *
+	 * @return String identifier
+	 */
+	public String toString() {
+		return title;
+	}
+	
+	/**
+	 * Returns the server instance associated with this container.
+	 *
+	 * @return the associated server connection
+	 */
+	public Server getServer() {
+		return null;
+	}
+	
+	/**
+	 * Closes this container (and it's associated frame).
+	 */
+	public void close() {
+		plugin.delWindow(this);
+		myWindow.setVisible(false);
+		Main.getUI().getMainWindow().delChild(myWindow);
+		WindowManager.removeWindow(myWindow);
+	}
+}
---
+++ src/com/dmdirc/addons/dcc/DCCPlugin.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2006-2007 Chris Smith, Shane Mc Cormack, Gregory Holmes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.addons.dcc;
+
+import com.dmdirc.commandparser.CommandManager;
+import com.dmdirc.plugins.Plugin;
+import com.dmdirc.ui.swing.JWrappingLabel;
+import com.dmdirc.ui.WindowManager;
+import com.dmdirc.Main;
+import com.dmdirc.ui.swing.components.Frame;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import javax.swing.SwingConstants;
+
+/**
+ * This plugin adds DCC to dmdirc
+ *
+ * @author Shane 'Dataforce' McCormack
+ * @version $Id: DCCPlugin.java 969 2007-04-30 18:38:20Z ShaneMcC $
+ */
+public final class DCCPlugin extends Plugin {
+	/** The DCCCommand we created */
+	private DCCCommand command = null;
+	
+	/** Our DCC Container window. */
+	private DCCFrame container;
+	
+	/** Child Frames */
+	private List<DCCFrame> childFrames = new ArrayList<DCCFrame>();
+	
+	/**
+	 * Creates a new instance of the DCC Plugin.
+	 */
+	public DCCPlugin() {
+		super();
+	}
+	
+	/**
+	 * Create the container window
+	 */
+	protected void createContainer() {
+		container = new DCCFrame(this, "DCCs"){};
+		JWrappingLabel label = new JWrappingLabel("This is a placeholder window to group DCCs together", SwingConstants.CENTER);
+		((Frame)container.getFrame()).getContentPane().add(label);
+		WindowManager.addWindow(container.getFrame());
+	}
+	
+	/**
+	 * Add a window to the container window
+	 *
+	 * @param window Window to remove
+	 */
+	protected void addWindow(final DCCFrame window) {
+		if (window == container) { return; }
+		if (container == null) { createContainer(); }
+		
+		WindowManager.addWindow(container.getFrame(), window.getFrame());
+		childFrames.add(window);
+	}
+	
+	/**
+	 * Remove a window from the container window
+	 *
+	 * @param window Window to remove
+	 */
+	protected void delWindow(final DCCFrame window) {
+		if (container == null) { return; }
+		if (window == container) {
+			container = null;
+			for (DCCFrame win : childFrames) {
+				if (win != window) {
+					win.close();
+				}
+			}
+			childFrames.clear();
+		} else {
+			childFrames.remove(window);
+			if (childFrames.size() == 0) {
+				container.close();
+				container = null;
+			}
+		}
+	}
+	
+	/**
+	 * Called when the plugin is loaded.
+	 */
+	@Override
+	public void onLoad() {
+		command = new DCCCommand(this);
+	}
+	
+	/**
+	 * Called when this plugin is Unloaded
+	 */
+	@Override
+	public void onUnload() {
+		CommandManager.unregisterCommand(command);
+		container.close();
+	}
+	
+	/**
+	 * Get SVN Version information.
+	 *
+	 * @return SVN Version String
+	 */
+	public static String getSvnInfo() { return "$Id: DCCPlugin.java 969 2007-04-30 18:38:20Z ShaneMcC $"; }	
+}
+
