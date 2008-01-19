37e97c5e398a27861e302cd71e01caed27c933a6
Chris Smith <chris@dmdirc.com>
2008-01-19 15:16:49 +0000

    Some parser unit testing
    git-svn-id: http://svn.dmdirc.com/trunk@3287 00569f92-eb28-0410-84fd-f71c24880f43

--- test/com/dmdirc/parser/IRCParserTest.java
+++ test/com/dmdirc/parser/IRCParserTest.java
@@ -30,11 +30,12 @@ import com.dmdirc.parser.callbacks.interfaces.INumeric;
 import com.dmdirc.parser.callbacks.interfaces.IPost005;
 import com.dmdirc.parser.callbacks.interfaces.IServerError;
 import java.util.Arrays;
+import javax.net.ssl.TrustManager;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class IRCParserTest extends junit.framework.TestCase {
-    
+
     @Test
     public void testIssue042() {
         boolean res = false;
@@ -79,15 +80,15 @@ public class IRCParserTest extends junit.framework.TestCase {
             final String asciiU = asciiParser.toUpperCase(testcase[1]);
             final String rfcU = rfcParser.toUpperCase(testcase[2]);
             final String strictU = strictParser.toUpperCase(testcase[3]);
-            
+
             assertEquals(testcase[1], asciiL);
             assertEquals(testcase[2], rfcL);
             assertEquals(testcase[3], strictL);
-            
+
             assertTrue(asciiParser.equalsIgnoreCase(testcase[0], testcase[1]));
             assertTrue(rfcParser.equalsIgnoreCase(testcase[0], testcase[2]));
             assertTrue(strictParser.equalsIgnoreCase(testcase[0], testcase[3]));
-            
+
             assertEquals(testcase[0], asciiU);
             assertEquals(testcase[0], rfcU);
             assertEquals(testcase[0], strictU);
@@ -110,7 +111,7 @@ public class IRCParserTest extends junit.framework.TestCase {
         assertTrue(Arrays.equals(res2, new String[]{"a", "b", "c", "d e"}));
         assertTrue(Arrays.equals(res3, new String[]{":a", "b:c", "d e"}));
     }
-    
+
     @Test
     public void testSendConnectionStrings1() {
         final ServerInfo serverInfo = new ServerInfo("irc.testing.dmdirc", 6667, "");
@@ -118,15 +119,15 @@ public class IRCParserTest extends junit.framework.TestCase {
         myInfo.setNickname("Nickname");
         myInfo.setRealname("Real name");
         myInfo.setUsername("Username");
-        
+
         final TestParser parser = new TestParser(myInfo, serverInfo);
         parser.sendConnectionStrings();
-        
+
         assertEquals(2, parser.sentLines.size());
-        
+
         assertTrue("Should send nickname line",
                 Arrays.equals(parser.getLine(0), new String[]{"NICK", "Nickname"}));
-        
+
         final String[] userParts = parser.getLine(1);
         assertEquals("First token should be USER", "USER", userParts[0]);
         assertEquals("USER should contain username", myInfo.getUsername().toLowerCase(),
@@ -134,7 +135,7 @@ public class IRCParserTest extends junit.framework.TestCase {
         assertEquals("USER should contain server name", serverInfo.getHost(), userParts[3]);
         assertEquals("USER should contain real name", "Real name", userParts[4]);
     }
-    
+
     @Test
     public void testSendConnectionStrings2() {
         final ServerInfo serverInfo = new ServerInfo("irc.testing.dmdirc", 6667, "password");
@@ -142,26 +143,26 @@ public class IRCParserTest extends junit.framework.TestCase {
         myInfo.setNickname("Nickname");
         myInfo.setRealname("Real name");
         myInfo.setUsername("Username");
-        
+
         final TestParser parser = new TestParser(myInfo, serverInfo);
         parser.sendConnectionStrings();
-        
+
         assertEquals(3, parser.sentLines.size());
-        
+
         assertTrue("Should send password line",
                 Arrays.equals(parser.getLine(0), new String[]{"PASS", "password"}));
-    }    
-    
+    }
+
     @Test
     public void testPingPong() {
         final TestParser parser = new TestParser();
-        
+
         parser.injectLine("PING :flubadee7291");
-        
+
         assertTrue("Should reply to PINGs with PONGs",
                 Arrays.equals(parser.getLine(0), new String[]{"PONG", "flubadee7291"}));
     }
-    
+
     @Test
     public void testError() throws CallbackNotFoundException {
         final ISETest test = new ISETest();
@@ -169,12 +170,12 @@ public class IRCParserTest extends junit.framework.TestCase {
         final TestParser parser = new TestParser();
         parser.getCallbackManager().addCallback("onServerError", test);
         parser.injectLine("ERROR :You smell of cheese");
-        
+
         assertNotNull(test.message);
         assertEquals("ERROR message should be passed to callback",
                 "You smell of cheese", test.message);
     }
-    
+
     @Test
     public void testAuthNotices() throws CallbackNotFoundException {
         final INATest test = new INATest();
@@ -182,37 +183,37 @@ public class IRCParserTest extends junit.framework.TestCase {
         parser.getCallbackManager().addCallback("onNoticeAuth", test);
         parser.sendConnectionStrings();
         parser.injectLine("NOTICE AUTH :Random auth notice?");
-        
+
         assertNotNull(test.message);
         assertEquals("Random auth notice?", test.message);
-        
+
         test.message = null;
-        
+
         parser.injectLine(":us.ircnet.org 020 * :Stupid notice");
-        
+
         assertNotNull(test.message);
-        assertEquals("Stupid notice", test.message);        
+        assertEquals("Stupid notice", test.message);
     }
-    
+
     @Test
     public void testNumeric() throws CallbackNotFoundException {
         final INTest test = new INTest();
         final TestParser parser = new TestParser();
         parser.getCallbackManager().addCallback("onNumeric", test);
-        
+
         parser.injectLine(":server 001 nick :Hi there, nick");
-        
+
         assertEquals(1, test.numeric);
-        assertTrue(Arrays.equals(new String[]{":server", "001", "nick", "Hi there, nick"}, 
+        assertTrue(Arrays.equals(new String[]{":server", "001", "nick", "Hi there, nick"},
                 test.data));
     }
-    
+
     @Test
     public void testPost005() throws CallbackNotFoundException {
         final IPFTest test = new IPFTest();
         final TestParser parser = new TestParser();
         parser.getCallbackManager().addCallback("onPost005", test);
-        
+
         final String[] strings = {
             "NOTICE AUTH :Blah, blah",
             ":server 020 * :Blah! Blah!",
@@ -225,19 +226,19 @@ public class IRCParserTest extends junit.framework.TestCase {
             ":server 005 nick MAXNICKLEN=15 TOPICLEN=250 AWAYLEN=160 :are supported by this server",
             ":server 375 nick :zomg, motd!",
         };
-        
+
         for (String string : strings) {
             assertFalse("OnPost005 fired too early", test.done);
             parser.injectLine(string);
         }
-        
+
         assertTrue("OnPost005 not fired", test.done);
     }
-    
+
     @Test
     public void test005Parsing() {
         final TestParser parser = new TestParser();
-        
+
         final String[] strings = {
             ":server 001 nick :Welcome to the Testing IRC Network, nick",
             ":server 002 nick :Your host is server.net, running version foo",
@@ -246,19 +247,96 @@ public class IRCParserTest extends junit.framework.TestCase {
             ":server 005 nick WHOX WALLCHOPS WALLVOICES NETWORK=moo :are supported by this server",
             ":server 005 nick MAXNICKLEN=15 MAXLIST=b:10,e:22,I:45 :are supported by this server",
             ":server 375 nick :zomg, motd!",
-        };        
-        
+        };
+
         for (String string : strings) {
             parser.injectLine(string);
         }
-        
+
         assertEquals(10, parser.getMaxListModes('b'));
         assertEquals(22, parser.getMaxListModes('e'));
         assertEquals(45, parser.getMaxListModes('I'));
         assertEquals("getMaxListModes should return 0 for unknowns;", 0, parser.getMaxListModes('z'));
         assertEquals("moo", parser.getNetworkName());
+        assertEquals("server", parser.getServerName());
+    }
+
+    @Test
+    public void testBindIP() {
+        final TestParser parser = new TestParser();
+
+        parser.setBindIP("abc.def.ghi.123");
+        assertEquals("abc.def.ghi.123", parser.getBindIP());
+    }
+
+    @Test
+    public void testCreateFake() {
+        final TestParser parser = new TestParser();
+
+        parser.setCreateFake(false);
+        assertFalse(parser.getCreateFake());
+        parser.setCreateFake(true);
+        assertTrue(parser.getCreateFake());
+    }
+
+    @Test
+    public void testAutoListMode() {
+        final TestParser parser = new TestParser();
+
+        parser.setAutoListMode(false);
+        assertFalse(parser.getAutoListMode());
+        parser.setAutoListMode(true);
+        assertTrue(parser.getAutoListMode());
+    }
+
+    @Test
+    public void testRemoveAfterCallback() {
+        final TestParser parser = new TestParser();
+
+        parser.setRemoveAfterCallback(false);
+        assertFalse(parser.getRemoveAfterCallback());
+        parser.setRemoveAfterCallback(true);
+        assertTrue(parser.getRemoveAfterCallback());
+    }
+
+    @Test
+    public void testAddLastLine() {
+        final TestParser parser = new TestParser();
+
+        parser.setAddLastLine(false);
+        assertFalse(parser.getAddLastLine());
+        parser.setAddLastLine(true);
+        assertTrue(parser.getAddLastLine());
     }
     
+    @Test
+    public void testDisconnectOnFatal() {
+        final TestParser parser = new TestParser();
+
+        parser.setDisconnectOnFatal(false);
+        assertFalse(parser.getDisconnectOnFatal());
+        parser.setDisconnectOnFatal(true);
+        assertTrue(parser.getDisconnectOnFatal());
+    }    
+    
+    @Test
+    public void testTrustManager() {
+        final TestParser parser = new TestParser();
+        
+        assertTrue(Arrays.equals(parser.getDefaultTrustManager(), parser.getTrustManager()));
+        
+        parser.setTrustManager(new TrustManager[0]);
+        
+        assertTrue(Arrays.equals(new TrustManager[0], parser.getTrustManager()));
+    }
+    
+    @Test
+    public void testGetParam() {
+        assertEquals("abc def", TestParser.getParam("foo :abc def"));
+        assertEquals("bar :abc def", TestParser.getParam("foo :bar :abc def"));
+        assertEquals("abc def", TestParser.getParam("abc def"));
+    }
+
     @Test
     public void testIRCds() {
         doIRCdTest("u2.10.12.10+snircd(1.3.4)", "snircd");
@@ -268,25 +346,25 @@ public class IRCParserTest extends junit.framework.TestCase {
         doIRCdTest("Unreal3.2.6", "unreal");
         doIRCdTest("bahamut-1.8(04)", "bahamut");
     }
-    
+
     private void doIRCdTest(final String ircd, final String expected) {
         final TestParser parser = new TestParser();
-        
+
         String[] strings = {
             ":server 001 nick :Welcome to the Testing IRC Network, nick",
             ":server 002 nick :Your host is server.net, running version %s",
             ":server 003 nick :This server was created Sun Jan 6 2008 at 17:34:54 CET",
             ":server 004 nick server.net %s dioswkgxRXInP biklmnopstvrDcCNuMT bklov"
         };
-        
+
         for (String line : strings) {
             parser.injectLine(String.format(line, ircd));
         }
-        
+
         assertEquals(ircd, parser.getIRCD(false));
         assertEquals(expected.toLowerCase(), parser.getIRCD(true).toLowerCase());
     }
-    
+
     private class ISETest implements IServerError {
         String message = null;
 
@@ -294,15 +372,15 @@ public class IRCParserTest extends junit.framework.TestCase {
             message = sMessage;
         }
     }
-    
+
     private class INATest implements INoticeAuth {
         String message = null;
 
         public void onNoticeAuth(IRCParser tParser, String sData) {
             message = sData;
-        }   
+        }
     }
-    
+
     private class INTest implements INumeric {
         int numeric = 0;
         String[] data;
@@ -312,7 +390,7 @@ public class IRCParserTest extends junit.framework.TestCase {
             data = token;
         }
     }
-    
+
     private class IPFTest implements IPost005 {
         boolean done = false;
 
