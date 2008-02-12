e16341de98f6656bc2c34bb07509471ad88d6636
Chris Smith <chris@dmdirc.com>
2008-02-12 04:40:25 +0000

    Preferences are no longer saved if the setting doesn't validate Prevents problems such as issue 859 from occuring from use of the prefs dialog
    git-svn-id: http://svn.dmdirc.com/trunk@3453 00569f92-eb28-0410-84fd-f71c24880f43

--- src/com/dmdirc/config/prefs/PreferencesSetting.java
+++ src/com/dmdirc/config/prefs/PreferencesSetting.java
@@ -274,7 +274,9 @@ public class PreferencesSetting {
      * @return True if the setting has changed, false otherwise
      */
     public boolean save() {
-        if ((current != null && current.equals(original)) || (current == null && original == null)) {
+        if ((current != null && current.equals(original)) 
+                || (current == null && original == null)
+                || (validator != null && validator.validate(current).isFailure())) {
             return false;
         }
         
