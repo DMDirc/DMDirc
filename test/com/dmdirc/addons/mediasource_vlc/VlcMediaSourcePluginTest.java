ca93bf333ac09b4c772a23fb7d2624c77b76fb29
Chris Smith <chris@dmdirc.com>
2008-04-15 15:46:08 +0000

    Added basic unit test for VLC plugin Issue 1106
    git-svn-id: http://svn.dmdirc.com/trunk@3926 00569f92-eb28-0410-84fd-f71c24880f43

---
+++ test/com/dmdirc/addons/mediasource_vlc/VlcMediaSourcePluginTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2006-2008 Chris Smith, Shane Mc Cormack, Gregory Holmes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.addons.mediasource_vlc;
+
+import com.dmdirc.util.TextFile;
+import java.io.IOException;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class VlcMediaSourcePluginTest extends junit.framework.TestCase {
+
+    @Test
+    public void testProcessInformation1() throws IOException {
+        final VlcMediaSourcePlugin plugin = new VlcMediaSourcePlugin();
+        final TextFile index = new TextFile(getClass().getResourceAsStream("index-1.html"));
+        final TextFile info = new TextFile(getClass().getResourceAsStream("info-1.html"));
+        
+        plugin.parseInformation(info.getLines(), index.getLines());
+        
+        assertTrue(plugin.isPlaying());
+        assertEquals("Manic Street Preachers", plugin.getArtist());
+        assertEquals("Send Away The Tigers", plugin.getAlbum());
+        assertEquals("The Second Great Depression", plugin.getTitle());
+        assertEquals("128 kb/s", plugin.getBitrate());
+        assertEquals("VLC", plugin.getAppName());
+        assertEquals("mpga", plugin.getFormat());
+        assertEquals("249", plugin.getLength());
+        assertEquals("38", plugin.getTime());
+    }
+
+}
