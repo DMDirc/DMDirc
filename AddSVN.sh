fb6007ba3e8abb1884e2c88cbbc75d132b51f1f8
Shane Mc Cormack <shane@dmdirc.com>
2008-06-08 13:43:37 +0000

    AddSVN now works with non-gnu (BSD/OSX) sed
    git-svn-id: http://svn.dmdirc.com/trunk@4159 00569f92-eb28-0410-84fd-f71c24880f43

--- AddSVN.sh
+++ AddSVN.sh
@@ -10,12 +10,19 @@ fi;
 SVNREV=`$SVN info | grep Revision`
 SVNREV=${SVNREV##*: }
 
+GNUSED=`sed --version 2>&1 | grep GNU`
+if [ "" != "${GNUSED}" ]; then
+	PARAM="-r"
+else
+	PARAM="-E"
+fi;
+
 if [ "${1}" = "--pre" ]; then
 	# Substitute the version string
-	${SED} -r 's/int SVN_REVISION = /int SVN_REVISION = '${SVNREV}'; \/\/ /' ${PWD}/src/com/dmdirc/Main.java > ${PWD}/src/com/dmdirc/Main.java.tmp 2>/dev/null
+	${SED} ${PARAM} 's/int SVN_REVISION = /int SVN_REVISION = '${SVNREV}'; \/\/ /' ${PWD}/src/com/dmdirc/Main.java > ${PWD}/src/com/dmdirc/Main.java.tmp 2>/dev/null
 elif [ "${1}" = "--post" ]; then
 	# Unsubstitute the version string
-	${SED} -r 's/int SVN_REVISION = .* ([0-9]+);/int SVN_REVISION = \1;/' ${PWD}/src/com/dmdirc/Main.java > ${PWD}/src/com/dmdirc/Main.java.tmp 2>/dev/null
+	${SED} ${PARAM} 's/int SVN_REVISION = .* ([0-9]+);/int SVN_REVISION = \1;/' ${PWD}/src/com/dmdirc/Main.java > ${PWD}/src/com/dmdirc/Main.java.tmp #2>/dev/null
 fi;
 
 if [ -e ${PWD}/src/com/dmdirc/Main.java.tmp ]; then
