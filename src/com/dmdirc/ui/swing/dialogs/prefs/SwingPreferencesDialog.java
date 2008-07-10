60c256e8f2d5d9af7b54af833e57c69cb1ed3d2f
Shane Mc Cormack <shane@dmdirc.com>
2008-07-10 21:52:10 +0000

    Issue 1321 and Issue 1246 Ok/Cancel buttons can now be seen on OSX
    git-svn-id: http://svn.dmdirc.com/trunk@4262 00569f92-eb28-0410-84fd-f71c24880f43

--- src/com/dmdirc/ui/swing/dialogs/prefs/SwingPreferencesDialog.java
+++ src/com/dmdirc/ui/swing/dialogs/prefs/SwingPreferencesDialog.java
@@ -156,7 +156,7 @@ public final class SwingPreferencesDialog extends StandardDialog implements
 
         setLayout(new MigLayout("fillx, wmin 650, wmax 650, hmax 600"));
         add(tabList, "width 150, growy, spany");
-        add(mainPanel, "wrap, width 480, grow");
+        add(mainPanel, "wrap, width 480, grow, hmax 550");
         add(getLeftButton(), "split, right");
         add(getRightButton(), "right");
     }
