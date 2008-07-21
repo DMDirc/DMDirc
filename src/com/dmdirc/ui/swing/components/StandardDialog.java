eb5e3451b3c674632fef7efba23f46df4522017f
Shane Mc Cormack <shane@dmdirc.com>
2008-07-21 16:43:00 +0000

    Fixes issue 1452: Pressing the X in the prefs dialog to close it should behave the same as pressing the cancel button
    git-svn-id: http://svn.dmdirc.com/trunk@4371 00569f92-eb28-0410-84fd-f71c24880f43

--- src/com/dmdirc/ui/swing/components/StandardDialog.java
+++ src/com/dmdirc/ui/swing/components/StandardDialog.java
@@ -29,6 +29,8 @@ import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
 import javax.swing.JComponent;
@@ -174,6 +176,13 @@ public class StandardDialog extends JDialog  {
                 }
             }
         };
+        
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(final WindowEvent windowEvent) {
+                executeAction(StandardDialog.this.cancelButton);
+            }
+        });
+
         
         final KeyStroke escape = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0);
         final KeyStroke enter = KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0);
