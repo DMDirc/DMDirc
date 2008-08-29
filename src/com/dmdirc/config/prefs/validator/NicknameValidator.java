22d66780eebd19ad20e9e745842b8b4a32b57124
Gregory Holmes <greg@dmdirc.com>
2008-08-29 15:55:31 +0000

    fixes issue 567
    git-svn-id: http://svn.dmdirc.com/trunk@4594 00569f92-eb28-0410-84fd-f71c24880f43

---
+++ src/com/dmdirc/config/prefs/validator/NicknameValidator.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2006-2007 Chris Smith, Shane Mc Cormack, Gregory Holmes
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.config.prefs.validator;
+
+/**
+ * Validates nicknames.
+ */
+public class NicknameValidator implements Validator<String> {
+
+    /** Nickname regex. */
+    private static final String NICKNAME_REGEX = "[A-Za-z0-9\\[\\]{|}\\-\\^\\\\\\`\\_]+";
+    /** Failure reason. */
+    private static final String FAILURE_REASON = "Nickname must only contain letters, numbers and []{}|-^\\.`_";
+
+    /**
+     * Instantiates a new nicknames validator.
+     */
+    public NicknameValidator() {
+        //Do nothing
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public ValidationResponse validate(final String object) {
+        if (object.matches(NICKNAME_REGEX)) {
+            return new ValidationResponse();
+        } else {
+            return new ValidationResponse(FAILURE_REASON);
+        }
+    }
+}
