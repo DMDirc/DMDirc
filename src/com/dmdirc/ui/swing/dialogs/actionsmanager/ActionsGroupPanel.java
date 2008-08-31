ce2a4426394c24177ea7284ce7dc058a9def5cd2
Gregory Holmes <greg@dmdirc.com>
2008-08-31 18:41:01 +0000

    import-- does not impact functionality
    git-svn-id: http://svn.dmdirc.com/trunk@4634 00569f92-eb28-0410-84fd-f71c24880f43

--- src/com/dmdirc/ui/swing/dialogs/actionsmanager/ActionsGroupPanel.java
+++ src/com/dmdirc/ui/swing/dialogs/actionsmanager/ActionsGroupPanel.java
@@ -35,8 +35,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-
 import java.util.ArrayList;
+
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
