ed11a510dabdbbd22966e91a4e365757fcba4582
Chris Smith <chris@dmdirc.com>
2008-09-18 19:05:20 +0000

    Add WINDOW_TITLE. Add a billion @Overrides. Remove stupid abstract methods. Fixes issue 1851
    git-svn-id: http://svn.dmdirc.com/trunk@4787 00569f92-eb28-0410-84fd-f71c24880f43

--- src/com/dmdirc/actions/CoreActionComponent.java
+++ src/com/dmdirc/actions/CoreActionComponent.java
@@ -50,54 +50,71 @@ public enum CoreActionComponent implements ActionComponent {
     /** Returns the name of the server. */
     SERVER_NAME {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) { return ((Server) argument).getName(); }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return Server.class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return String.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "name"; }
     },
     
     /** Returns the network of the server. */
     SERVER_NETWORK {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) { return ((Server) argument).getNetwork(); }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return Server.class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return String.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "network"; }
     },
     
     /** Returns the away reason for the server. */
     SERVER_MYAWAYREASON {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) { return ((Server) argument).getAwayMessage(); }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return Server.class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return String.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "away reason"; }
     },
     
     /** Returns the channel umodes for the server. */
     SERVER_CHANNELUMODES {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) { return ((Server) argument).getParser().getPrefixModes(); }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return Server.class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return String.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "list of channel usermodes"; }
     },    
     
     /** Returns the nickname for the server. */
     SERVER_MYNICKNAME {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) {
             final Server server = (Server) argument;
             
@@ -116,281 +133,372 @@ public enum CoreActionComponent implements ActionComponent {
             }
         }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return Server.class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return String.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "nickname"; }
     },
     
     /** Returns the name of the channel. */
     CHANNEL_NAME {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) { return ((Channel) argument).getChannelInfo().getName(); }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return Channel.class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return String.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "name"; }
     },
     
     /** Returns the notification colour of the channel. */
     CHANNEL_COLOUR {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) { return ((Channel) argument).getNotification(); }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return Channel.class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return Color.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "notification colour"; }
     },
     
     /** Returns the name of a client. */
     CLIENT_NAME {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) { return ((ClientInfo) argument).getNickname(); }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return ClientInfo.class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return String.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "nickname"; }
     },
     
     /** Returns the host of a client. */
     CLIENT_HOST {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) { return ((ClientInfo) argument).getHost(); }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return ClientInfo.class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return String.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "host"; }
     },
     
     /** Returns the name of a client. */
     USER_NAME {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) { return ((ChannelClientInfo) argument).getNickname(); }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return ChannelClientInfo.class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return String.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "nickname"; }
     },
     
     /** Returns the modes of a client. */
     USER_MODES {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) { return ((ChannelClientInfo) argument).getChanModeStr(false); }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return ChannelClientInfo.class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return String.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "modes"; }
     },
     
     /** Returns the host of a client. */
     USER_HOST {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) { return ((ChannelClientInfo) argument).getClient().getHost(); }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return ChannelClientInfo.class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return String.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "host"; }
     },
     
     /** Returns the number of common channels the client is on. */
     USER_COMCHANS {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) { return Integer.valueOf(((ChannelClientInfo) argument).getClient().channelCount()); }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return ChannelClientInfo.class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return Integer.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "number of common channels"; }
     },
     
     /** Returns the content of a string. */
     STRING_STRING {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) { return argument; }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return String.class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return String.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "content"; }
     },
     
     /** Returns the content of a string, stripped of formatting. */
     STRING_STRIPPED {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) { return Styliser.stipControlCodes((String) argument); }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return String.class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return String.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "content (without formatting)"; }
     },    
     
     /** Returns the length of a string. */
     STRING_LENGTH {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) { return ((String) argument).length(); }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return String.class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return Integer.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "length"; }
     },
     
     /** Returns the size of a string array. */
     STRINGARRAY_LENGTH {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) { return Integer.valueOf(((String[]) argument).length); }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return String[].class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return Integer.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "size"; }
     },
     
     /** Returns the readable representation of a date. */
     CALENDAR_FULLSTRING {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) { return ((GregorianCalendar) argument).getTime().toString(); }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return Calendar.class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return String.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "full date"; }
     },
     
     /** Returns the name of the key that was pressed. */
     KEYEVENT_KEYNAME {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) { return KeyEvent.getKeyText(((KeyStroke) argument).getKeyCode()); }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return KeyStroke.class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return String.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "key name"; }
     },
     
     /** Returns the state of the control key for a key press event. */
     KEYEVENT_CTRLSTATE {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) {
             return Boolean.valueOf((((KeyStroke) argument).getModifiers() & KeyEvent.CTRL_DOWN_MASK) != 0);
         }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return KeyStroke.class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return Boolean.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "control key state"; }
     },
     
     /** Returns the state of the shift key for a key press event. */
     KEYEVENT_SHIFTSTATE {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) {
             return Boolean.valueOf((((KeyStroke) argument).getModifiers() & KeyEvent.SHIFT_DOWN_MASK) != 0);
         }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return KeyStroke.class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return Boolean.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "shift key state"; }
     },
     
     /** Returns the state of the shift key for a key press event. */
     KEYEVENT_ALTSTATE {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) {
             return Boolean.valueOf((((KeyStroke) argument).getModifiers() & KeyEvent.ALT_DOWN_MASK) != 0);
         }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return KeyStroke.class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return Boolean.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "alt key state"; }
     },
     
     /** Returns the host of the query. */
     QUERY_HOST {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) { return ((Query) argument).getHost(); }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return Query.class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return String.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "host"; }
     },
     
     /** Returns the host of the query. */
     QUERY_NICK {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) { return ((Query) argument).toString(); }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return Query.class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return String.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "nick"; }
     },
     
     /** Returns the notification colour of the query. */
     QUERY_COLOUR {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) { return ((Query) argument).getNotification(); }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return Query.class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return Color.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "notification colour"; }
+    },
+
+    /** The name of a window. */
+    WINDOW_NAME {
+        /** {@inheritDoc} */
+        @Override
+        public Object get(final Object argument) { return ((FrameContainer) argument).toString(); }
+        /** {@inheritDoc} */
+        @Override
+        public Class appliesTo() { return FrameContainer.class; }
+        /** {@inheritDoc} */
+        @Override
+        public Class getType() { return String.class; }
+        /** {@inheritDoc} */
+        @Override
+        public String getName() { return "name"; }
     },
     
     /** Returns the notification colour of the window. */
     WINDOW_COLOUR {
         /** {@inheritDoc} */
+        @Override
         public Object get(final Object argument) { return ((FrameContainer) argument).getNotification(); }
         /** {@inheritDoc} */
+        @Override
         public Class appliesTo() { return FrameContainer.class; }
         /** {@inheritDoc} */
+        @Override
         public Class getType() { return Color.class; }
         /** {@inheritDoc} */
+        @Override
         public String getName() { return "notification colour"; }
     };
-    
-    /** {@inheritDoc} */
-    public abstract Object get(Object argument);
-    
-    /** {@inheritDoc} */
-    public abstract Class appliesTo();
-    
-    /** {@inheritDoc} */
-    public abstract Class getType();
-    
-    /** {@inheritDoc} */
-    public abstract String getName();
-    
+        
 }
