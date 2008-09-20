aba374e63a3e8765749dc7a7b9585cbc97b7cb86
Chris Smith <chris@dmdirc.com>
2008-09-20 21:41:47 +0000

    Partial unit testing for our latest broken dialog
    git-svn-id: http://svn.dmdirc.com/trunk@4812 00569f92-eb28-0410-84fd-f71c24880f43

---
+++ test/com/dmdirc/ui/swing/dialogs/sslcertificate/SSLCertificateDialogTest.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2006-2007 Chris Smith, Shane Mc Cormack, Gregory Holmes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.ui.swing.dialogs.sslcertificate;
+
+import com.dmdirc.harness.ui.ClassFinder;
+import com.dmdirc.harness.ui.TestSSLCertificateDialogModel;
+import com.dmdirc.harness.ui.UIClassTestRunner;
+import com.dmdirc.harness.ui.UITestIface;
+import com.dmdirc.ui.IconManager;
+
+import com.dmdirc.ui.swing.UIUtilities;
+import java.awt.Component;
+import java.util.Arrays;
+
+import javax.swing.Icon;
+import javax.swing.JLabel;
+import javax.swing.JList;
+
+import javax.swing.JScrollPane;
+import javax.swing.border.TitledBorder;
+import org.fest.swing.core.EventMode;
+import org.fest.swing.driver.BasicJListCellReader;
+import org.fest.swing.fixture.DialogFixture;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+
+@RunWith(UIClassTestRunner.class)
+public class SSLCertificateDialogTest implements UITestIface {
+
+    private DialogFixture window;
+
+    @Before
+    public void setUp() {
+        UIUtilities.initUISettings();
+    }
+
+    @After
+    public void tearDown() {
+        if (window != null) {
+            window.cleanUp();
+        }
+    }
+
+    @Test
+    public void testTicksAndCrosses() {
+        setupWindow();
+
+        assertTrue(Arrays.equals(new String[]{
+            "first cert",
+            "second cert",
+            "invalid cert",
+            "trusted cert",
+            "invalid+trusted"
+        }, window.list().contents()));
+
+        assertTrue(Arrays.equals(new String[]{
+            "nothing",
+            "nothing",
+            "cross",
+            "tick",
+            "cross"
+        },window.list().cellReader(new CertificateListCellReader()).contents()));
+    }
+
+    @Test @Ignore
+    public void testSelection() throws InterruptedException {
+        setupWindow();
+
+        window.list().requireSelection("first cert");
+
+        Thread.sleep(10000);
+        
+        for (String cert : window.list().contents()) {
+            window.list().selectItem(cert).requireSelection(cert);
+
+            assertEquals("Information for " + cert, ((TitledBorder) window
+                    .scrollPane(new ClassFinder<JScrollPane>(CertificateInfoPanel.class, null))
+                    .target.getBorder()).getTitle());
+        }
+    }
+
+    protected void setupWindow() {
+        window = new DialogFixture(new SSLCertificateDialog(null,
+                new TestSSLCertificateDialogModel()));
+
+        window.robot.settings().eventMode(EventMode.AWT);
+        window.show();
+    }
+
+    public static junit.framework.Test suite() {
+        return new junit.framework.JUnit4TestAdapter(SSLCertificateDialogTest.class);
+    }
+
+    private static class CertificateListCellReader extends BasicJListCellReader {
+
+        public String valueAt(JList arg0, int arg1) {
+            final Component c = cellRendererComponent(arg0, arg1);
+
+            final Icon target = ((JLabel) c).getIcon();
+            
+            for (String icon : new String[]{"tick", "cross", "nothing"}) {
+                if (target == IconManager.getIconManager().getIcon(icon)) {
+                    return icon;
+                }
+            }
+
+            return "?";
+        }
+
+    }
+
+}
