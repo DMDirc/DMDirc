022d65f4baf4e065ffdb94b24474cf6c1bd14850
Chris Smith <chris@dmdirc.com>
2008-09-28 01:39:08 +0100

    Remove unused import

--- src/com/dmdirc/config/Identity.java
+++ src/com/dmdirc/config/Identity.java
@@ -35,7 +35,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
-import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
