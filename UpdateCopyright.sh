ad6cddbba0d5cb30009597c8280e445027922506
Gregory Holmes <greg@dmdirc.com>
2009-01-02 12:41:53 +0000

    Update copyright header to 2009

--- UpdateCopyright.sh
+++ UpdateCopyright.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # Update Copyright headers in all files.
 
-OLD="2006-2007"
-NEW="2006-2008"
+OLD="2006-2008"
+NEW="2006-2009"
 
 grep -iR "${OLD}" ./* | grep -v /.svn/ | grep -v "Binary file" | grep -v UpdateCopyright.sh | grep -v update.sh | awk -F: '{print "echo \"Updating \\\""$1"\\\"\"\nsed -i \"s/'${OLD}'/'${NEW}'/g\" \""$1"\""}' > update.sh
 sh update.sh
