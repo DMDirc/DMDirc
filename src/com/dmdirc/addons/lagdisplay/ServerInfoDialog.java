d041640da29fe79d9bfea43aabf406baaab04ac1
Chris Smith <chris@dmdirc.com>
2009-02-17 01:45:09 +0000

    Lag display popup will now be aligned properly with the label, rather than a few pixels off to the left if the label is near the client edge. Fixes issue 2165.

--- src/com/dmdirc/addons/lagdisplay/ServerInfoDialog.java
+++ src/com/dmdirc/addons/lagdisplay/ServerInfoDialog.java
@@ -103,8 +103,9 @@ public class ServerInfoDialog extends StandardDialog {
 
         final Point point = parent.getLocationOnScreen();
         point.translate(parent.getWidth() / 2 - this.getWidth() / 2, - this.getHeight());
-        final int maxX = SwingController.getMainFrame().getLocationOnScreen().x
-                + SwingController.getMainFrame().getWidth() - 10 - getWidth();
+        final int maxX = Math.max(SwingController.getMainFrame().getLocationOnScreen().x
+                + SwingController.getMainFrame().getWidth() - 10 - getWidth(),
+                parent.getLocationOnScreen().x + parent.getWidth() - 1 - getWidth());
         point.x = Math.min(maxX, point.x);
         setLocation(point);
 
