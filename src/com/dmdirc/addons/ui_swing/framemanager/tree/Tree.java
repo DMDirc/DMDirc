560bb69b61531a630ccd334fb4deb4af2ddb7cae
Gregory Holmes <greg@dmdirc.com>
2009-03-11 18:22:09 +0000

    ok fine, i wont try and fix that

--- src/com/dmdirc/addons/ui_swing/framemanager/tree/Tree.java
+++ src/com/dmdirc/addons/ui_swing/framemanager/tree/Tree.java
@@ -128,15 +128,15 @@ public class Tree extends JTree implements TreeSelectionListener,
     public void setSelection(final TreePath path) {
         if (this.path != null) {
             final TreePath pathCopy = new TreePath(path);
-            setSelectionPath(pathCopy);
-            if (!this.path.equals(pathCopy)) {
+            setSelectionPath(path);
+            if (!this.path.equals(path)) {
                 UIUtilities.invokeLater(new Runnable() {
 
                     /** {@inheritDoc} */
                     @Override
                     public void run() {
-                        setTreePath(pathCopy);
-                        ((TreeViewNode) pathCopy.getLastPathComponent()).getFrameContainer().activateFrame();
+                        setTreePath(path);
+                        ((TreeViewNode) path.getLastPathComponent()).getFrameContainer().activateFrame();
                     }
                 });
             }
