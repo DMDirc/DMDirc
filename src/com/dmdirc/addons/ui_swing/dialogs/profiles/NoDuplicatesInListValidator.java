55dd5f0d86535427b2e2bf714b64da15fb86b7dc
Gregory Holmes <greg@dmdirc.com>
2009-05-04 01:49:07 +0100

    yay for files

---
+++ src/com/dmdirc/addons/ui_swing/dialogs/profiles/NoDuplicatesInListValidator.java
@@ -0,0 +1,56 @@
+/*
+ * 
+ * Copyright (c) 2006-2008 Chris Smith, Shane Mc Cormack, Gregory Holmes
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.addons.ui_swing.dialogs.profiles;
+
+import com.dmdirc.config.prefs.validator.ValidationResponse;
+import com.dmdirc.config.prefs.validator.Validator;
+
+/**
+ * Validator to check for duplicate values in a list.
+ */
+public class NoDuplicatesInListValidator implements Validator<String> {
+
+    /** List to validate. */
+    private ProfileListModel model;
+
+    /**
+     * Creates a new validator.
+     *
+     * @param model Model to validate
+     */
+    public NoDuplicatesInListValidator(final ProfileListModel model) {
+        this.model = model;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public ValidationResponse validate(final String object) {
+        if (model.contains(object)) {
+            return new ValidationResponse("Value is a duplicate");
+        } else {
+            return new ValidationResponse();
+        }
+    }
+
+}
