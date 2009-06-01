6bb97c33f676f5779f799f40b869a302f0d908c9
Chris Smith <chris@dmdirc.com>
2009-06-02 00:47:37 +0100

    Command name validator. Issue 2611

---
+++ src/com/dmdirc/config/prefs/validator/CommandNameValidator.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2006-2009 Chris Smith, Shane Mc Cormack, Gregory Holmes
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.config.prefs.validator;
+
+import com.dmdirc.commandparser.CommandManager;
+import java.util.regex.Pattern;
+
+/**
+ * Validates command names.
+ *
+ * @since 0.6.3m1rc3
+ */
+public class CommandNameValidator extends ValidatorChain<String> {
+
+    /**
+     * Instantiates a new command name validator.
+     */
+    @SuppressWarnings("unchecked")
+    public CommandNameValidator() {
+        super(new RegexStringValidator("^[^\\s]*$", "Cannot contain spaces"),
+                new RegexStringValidator("^[^"
+                + Pattern.quote(String.valueOf(CommandManager.getCommandChar()))
+                + "].*$", "Cannot start with a " + CommandManager.getCommandChar()));
+    }
+
+}
