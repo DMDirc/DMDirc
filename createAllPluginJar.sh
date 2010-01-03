1661166ce8c81a867d07963f0a77255da576179e
Shane Mc Cormack <shane@dmdirc.com>
2010-01-03 01:56:30 +0000

    Remove Plugins, update stuff to use new plugins submodule.
    Change-Id: If6fb44e5e9e359d1eb87ba7458767c1d150badd6

--- createAllPluginJar.sh
+++ createAllPluginJar.sh
@@ -3,8 +3,8 @@
 # it will create plugin jars for all the plugins in the addons directory.
 
 if [ ${?} = "0" ]; then
-	for dir in `ls -1 src/com/dmdirc/addons`; do
-		if [ -e "src/com/dmdirc/addons/${dir}/.ignore" ]; then
+	for dir in `ls -1 modules/plugins/src/com/dmdirc/addons`; do
+		if [ -e "modules/plugins/src/com/dmdirc/addons/${dir}/.ignore" ]; then
 			echo "------"
 			echo "Not building: ${dir}"
 			echo "------"
