ed298a47c6456e67a4a557c083630bd2bd439394
Shane Mc Cormack <shane@dmdirc.com>
2010-01-11 13:27:01 +0000

    Rather than just including an old version of the functions in the launcher, build in functions.sh from the installer. This really negates the need to even include functions.sh when it exists but I'll leave that in for now.
    Change-Id: I579dff3cae8930f43eea04780fa60cb55738a7d3

--- launcher/createUpdate.sh
+++ launcher/createUpdate.sh
@@ -6,9 +6,46 @@ LAUNCHERUPDATEDIR="/home/dmdirc/www/updates/launchers/"
 UNIXVERSION=`cat ${LAUNCHERDIR}/unix/DMDirc.sh | grep LAUNCHERVERSION= | awk -F\" '{print $2}'`
 WINDOWSVERSION=`cat ${LAUNCHERDIR}/windows/DMDirc.dpr | grep "launcherVersion: String =" | awk -F\' '{print $2}'`
 
+# Find out what params we should pass to things.
+# Solaris has a nice and ancient version of grep in /usr/bin
+grep -na "" /dev/null >/dev/null 2>&1
+if [ $? -eq 2 ]; then
+	GREPOPTS="-n"
+else
+	GREPOPTS="-na"
+fi;
+# Solaris also has a crappy version of tail!
+tail -n +1 /dev/null >/dev/null 2>&1
+if [ $? -eq 2 ]; then
+	TAILOPTS="+"
+else
+	TAILOPTS="-n +"
+fi;
+
 if [ ! -e "${LAUNCHERUPDATEDIR}/unix-${UNIXVERSION}.sh" ]; then
 	echo "Creating Launcher Update: unix-${UNIXVERSION}";
-	cp -${LAUNCHERDIR}/unix/DMDirc.sh ${LAUNCHERUPDATEDIR}/unix-${UNIXVERSION}.sh
+	
+	FUNCTIONSFILE="${LAUNCHERDIR}/../installer/linux/functions.sh"
+	SRCFILE=${LAUNCHERDIR}/unix/DMDirc.sh
+	DESTFILE=${LAUNCHERUPDATEDIR}/unix-${UNIXVERSION}.sh
+	
+	if [ -e "${FUNCTIONSFILE}" ]; then
+		FUNCTIONSLINE=`grep ${GREPOPTS} "^###FUNCTIONS_FILE###$" ${SRCFILE}`
+		if [ "${FUNCTIONSLINE}" == "" ]; then
+			echo "    Functions already built into launcher."
+			cp ${SRCFILE} ${DESTFILE}
+		else
+			echo "    Including functions.sh into launcher."
+			FUNCTIONSLINE=$((${FUNCTIONSLINE%%:*} + 0))
+			
+			head -n ${FUNCTIONSLINE} ${SRCFILE} > ${DESTFILE}
+			cat functions.sh >> ${DESTFILE}
+			echo "" >> ${DESTFILE}
+			tail ${TAILOPTS}$((${FUNCTIONSLINE%%:*} + 1)) ${SRCFILE} >> ${DESTFILE}
+		fi;
+	else
+		echo "    Unable to create unix launcher update, no functions.sh found."
+	fi;
 fi;
 
 if [ ! -e "${LAUNCHERUPDATEDIR}/windows-${WINDOWSVERSION}.zip" ]; then
