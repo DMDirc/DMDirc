5be2f3b3ed92fc4a8285efc96d8d39be233ffa82
Chris Smith <chris@dmdirc.com>
2010-01-27 22:08:56 +0000

    Add channel topic listeners
    Change-Id: Id72f5f4ca191592ff0b04883e62de2263b3ebbdc

---
+++ src/com/dmdirc/interfaces/TopicChangeListener.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2006-2010 Chris Smith, Shane Mc Cormack, Gregory Holmes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.interfaces;
+
+import com.dmdirc.Channel;
+import com.dmdirc.Topic;
+
+/**
+ * An interface for objects interested in receiving topic change events from
+ * a {@link Channel}.
+ *
+ * @since 0.6.3
+ * @author chris
+ */
+public interface TopicChangeListener {
+
+    /**
+     * Called whenever a topic is changed.
+     *
+     * @param channel The channel whose topic has been changed
+     * @param topic The new topic of the channel
+     */
+    void topicChanged(final Channel channel, final Topic topic);
+
+}
