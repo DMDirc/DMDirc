fe88a1cdfe62529591d454edd41f34356ca54576
Chris Smith <chris@dmdirc.com>
2010-01-28 01:28:23 +0000

    Action manager test now loads actions properly
    Fixes issue 3646, fixes issue 3647
    
    Change-Id: Ib19add4d3d82bc86db9e976a5a50031c2ac58eea
    Reviewed-on: http://gerrit.dmdirc.com/670
    Automatic-Compile: DMDirc Local Commits <dmdirc@googlemail.com>
    Reviewed-by: Shane Mc Cormack <shane@dmdirc.com>

--- test/com/dmdirc/actions/ActionManagerTest.java
+++ test/com/dmdirc/actions/ActionManagerTest.java
@@ -37,6 +37,7 @@ public class ActionManagerTest {
     public static void setUpClass() throws Exception {
         IdentityManager.load();
         ActionManager.init();
+        ActionManager.loadActions();
         
         tearDownClass();
     }
