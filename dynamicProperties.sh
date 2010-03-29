be41f59b698611eb8af5531ce24dcb3bd337910e
Shane Mc Cormack <shane@dmdirc.com>
2010-03-29 01:05:34 +0100

    Netbeans doesn't like dynamic.properties, so hax private.properties aswell.
    Change-Id: Iafbbb18a2eb39972f7398a8871e68ca0ca276543
    Reviewed-on: http://gerrit.dmdirc.com/1212
    Automatic-Compile: DMDirc Local Commits <dmdirc@googlemail.com>
    Reviewed-by: Gregory Holmes <greg@dmdirc.com>

---
+++ dynamicProperties.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SOURCE="${1}"
+TARGET="${2}"
+
+if [ "" = "${TARGET}" -o ! -e "${SOURCE}" ]; then
+	echo "Usage: ${0} dynamic.properties private.properties";
+else
+	# Make sure target exists.
+	if [ ! -e "${TARGET}" ]; then
+		mkdir -P `dirname "${TARGET}"`
+		touch "${TARGET}"
+	fi;
+	
+	# Remove existing private.properties line
+	sed '/^private\.classpath[\s]*=.*$/d' -i "${TARGET}"
+
+	# Add new one.
+	cat "${SOURCE}" | grep "^private\.classpath" >> "${TARGET}"
+fi;
