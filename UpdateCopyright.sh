fd83b6b2a939647bd4ca4c20a0c85ccb2d1b2655
Shane Mc Cormack <shane@dmdirc.com>
2010-12-31 23:45:10 +0000

    Update Copyright
    Change-Id: I7f63fdd02ea0109fe31e55c44b078edae3862f32
    Reviewed-on: http://gerrit.dmdirc.com/1726
    Reviewed-by: Chris Smith <chris@dmdirc.com>
    Automatic-Compile: DMDirc Local Commits <dmdirc@googlemail.com>

--- UpdateCopyright.sh
+++ UpdateCopyright.sh
@@ -2,8 +2,8 @@
 # Update Copyright headers in all files.
 
 THISYEAR=`date +%Y`
-OLD="2006-"$((${THISYEAR} - 1))
-NEW="2006-"${THISYEAR}
+OLD="2006-"${THISYEAR}
+NEW="2006-"$((${THISYEAR} + 1))
 
 grep -iRI "${OLD}" ./* | grep -v /.git/ | grep -v UpdateCopyright.sh | grep -v update.sh | awk -F: '{print "echo \"Updating \\\""$1"\\\"\"\nsed -i \"s/'${OLD}'/'${NEW}'/g\" \""$1"\""}' > update.sh
 sh update.sh
--- launcher/unix/DMDirc.sh
+++ launcher/unix/DMDirc.sh
@@ -3,7 +3,7 @@
 # This script launches dmdirc and attempts to update the jar file if needed.
 #
 # DMDirc - Open Source IRC Client
-# Copyright (c) 2006-2010 Chris Smith, Shane Mc Cormack, Gregory Holmes
+# Copyright (c) 2006-2011 Chris Smith, Shane Mc Cormack, Gregory Holmes
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -164,7 +164,7 @@ BSDJava2="/usr/local/diablo-jdk1.6.0/jre/bin/java"
 echo "---------------------"
 echo "DMDirc - Open Source IRC Client"
 echo "Launcher Version: ${LAUNCHERVERSION}"
-echo "Copyright (c) 2006-2010 Chris Smith, Shane Mc Cormack, Gregory Holmes"
+echo "Copyright (c) 2006-2011 Chris Smith, Shane Mc Cormack, Gregory Holmes"
 echo "---------------------"
 if [ "${ISOSX}" = "1" ]; then
 	echo "Running on OS X."
