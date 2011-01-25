1c3a552c3d0bd3cdece0762b9e8f89ff11d0c6b2
Greg Holmes <greg@dmdirc.com>
2011-01-25 13:42:16 +0000

    Create message component for status bar messages
    Issue CLIENT-211
    
    Change-Id: I8d1d92e3f23ea3c44e84f2f7a06fcc9173300cd6
    Reviewed-on: http://gerrit.dmdirc.com/1849
    Automatic-Compile: DMDirc Local Commits <dmdirc@googlemail.com>
    Reviewed-by: Chris Smith <chris@dmdirc.com>

---
+++ src/com/dmdirc/ui/StatusMessage.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2006-2011 Chris Smith, Shane Mc Cormack, Gregory Holmes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.ui;
+
+import com.dmdirc.ui.interfaces.StatusMessageNotifier;
+
+/**
+ * Describes a status bar message to be presented to a user.
+ */
+public class StatusMessage {
+
+    /** Icon type for the message. */
+    private final String iconType;
+    /** Mesage to display. */
+    private final String message;
+    /** Message notifier. */
+    private final StatusMessageNotifier messageNotifier;
+    /** Timeout when initially displayed. */
+    private final int timeout;
+
+    /**
+     * Creates a new statusbar message.
+     *
+     * @param iconType Icon type to use for the message
+     * @param message Message to show
+     * @param messageNotifier Optional notifier
+     */
+    public StatusMessage(final String iconType, final String message,
+            final StatusMessageNotifier messageNotifier, final int timeout) {
+        this.iconType = iconType;
+        this.message = message;
+        this.messageNotifier = messageNotifier;
+        this.timeout = timeout;
+    }
+
+    /**
+     * Get the value of message
+     *
+     * @return the value of message
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * Get the value of iconType
+     *
+     * @return the value of iconType
+     */
+    public String getIconType() {
+        return iconType;
+    }
+
+    /**
+     * Get the value of messageNotifier
+     *
+     * @return the value of messageNotifier
+     */
+    public StatusMessageNotifier getMessageNotifier() {
+        return messageNotifier;
+    }
+    
+    /**
+     * Get the value of timeout
+     * 
+     * @return the value of timeout
+     */
+    public int getTimeout() {
+        return timeout;
+    }
+}
