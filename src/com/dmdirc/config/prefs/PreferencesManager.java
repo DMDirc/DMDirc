d04e2bea4fca671c01a9213f3590628b240506be
Greg Holmes <greg@dmdirc.com>
2011-03-05 01:07:47 +0000

    Change header?
    Change-Id: I635e0d111ed182713132145e4d5fb1654f776ec4
    Reviewed-on: http://gerrit.dmdirc.com/2071
    Automatic-Compile: DMDirc Build Manager
    Reviewed-by: Chris Smith <chris@dmdirc.com>

--- src/com/dmdirc/config/prefs/PreferencesManager.java
+++ src/com/dmdirc/config/prefs/PreferencesManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006-2011 Chris Smith, Shane Mc Cormack, Gregory Holmes
+ * Copyright (c) 2006-2011 DMDirc Developers
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
