56d1406cdd0f94aaf1d87ca627c5ac22731d2041
Shane Mc Cormack <shane@dmdirc.com>
2012-01-01 01:32:04 +0000

    UpdateCopyright.sh now matches some more things that we missed before. When matching lines, check for the word "DMDirc" in the header aswell so as not to change other peoples copyright.
    Change-Id: Ibb44871d3c75e12bfc8acd8410d6043910d072c5
    Reviewed-on: http://gerrit.dmdirc.com/2301
    Automatic-Compile: DMDirc Build Manager
    Reviewed-by: Chris Smith <chris@dmdirc.com>

--- UpdateCopyright.sh
+++ UpdateCopyright.sh
@@ -4,5 +4,4 @@
 THISYEAR=`date +%Y`
 THISYEAR=$((${THISYEAR} + 1))
 
-find . -regextype posix-egrep -iregex '.*\.(java|sh|php|xml)$' -exec sed -i 's/\(Copyright.*\)2006-20[0-9][0-9]/\12006-'${THISYEAR}'/' {} \;
-
+find . -regextype posix-egrep -iregex '.*/(.*\.(java|sh|php|xml|dpr|nsh|html|htm)|dmdirc.license|DMDirc-Apple.c|DMDirc - MIT|copyright)$' -exec sed -i 's/\(Copyright.*\)2006-20[0-9][0-9]\(.*DMDirc.*\)/\12006-'${THISYEAR}'\2/' {} \;
