d1b2b98b56695c5de62e8dff5abb2722eb7c8f79
Greg Holmes <greg@dmdirc.com>
2012-02-23 00:06:16 +0000

    Add unit test for StatusMessage
    Change-Id: I7488d64d10147a95df5676a0a9ebb10574af7444
    Reviewed-on: http://gerrit.dmdirc.com/2449
    Automatic-Compile: DMDirc Build Manager
    Reviewed-by: Chris Smith <chris@dmdirc.com>

---
+++ test/com/dmdirc/ui/StatusMessageTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2006-2012 DMDirc Developers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.dmdirc.ui;
+
+import com.dmdirc.config.ConfigManager;
+import com.dmdirc.interfaces.ui.StatusMessageNotifier;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class StatusMessageTest {
+
+    @Test
+    public void testGetMessageShortConstructor() {
+        final ConfigManager config = mock(ConfigManager.class);
+        StatusMessage instance = new StatusMessage("test", config);
+        assertEquals("test", instance.getMessage());
+    }
+
+    @Test
+    public void testGetMessageLongConstructor() {
+        final ConfigManager config = mock(ConfigManager.class);
+        StatusMessage instance = new StatusMessage("icon", "test", null, 10, config);
+        assertEquals("test", instance.getMessage());
+    }
+
+    @Test
+    public void testGetIconType() {
+        final ConfigManager config = mock(ConfigManager.class);
+        StatusMessage instance = new StatusMessage("icon", "test", null, 10, config);
+        assertEquals("icon", instance.getIconType());
+    }
+
+    @Test
+    public void testGetMessageNotifierNull() {
+        final ConfigManager config = mock(ConfigManager.class);
+        StatusMessage instance = new StatusMessage("icon", "test", null, 10, config);
+        assertNull(instance.getMessageNotifier());
+    }
+
+    @Test
+    public void testGetMessageNotifierNotNull() {
+        final ConfigManager config = mock(ConfigManager.class);
+        final StatusMessageNotifier smn = mock(StatusMessageNotifier.class);
+        StatusMessage instance = new StatusMessage("icon", "test", smn, 10, config);
+        assertEquals(smn, instance.getMessageNotifier());
+    }
+
+    @Test
+    public void testGetTimeout() {
+        final ConfigManager config = mock(ConfigManager.class);
+        StatusMessage instance = new StatusMessage("icon", "test", null, 10, config);
+        assertEquals(10, instance.getTimeout());
+    }
+
+    @Test
+    public void testGetTimeoutFallback() {
+        final ConfigManager config = mock(ConfigManager.class);
+
+        when(config.getOptionInt("statusBar", "messageDisplayLength")).thenReturn(10);
+
+        StatusMessage instance = new StatusMessage("icon", "test", null, -1, config);
+        assertEquals(10, instance.getTimeout());
+    }
+}
