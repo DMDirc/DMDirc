67936bd5dee9e007bc3bda8d383befe57cd7b842
Chris Smith <chris@dmdirc.com>
2013-09-17 22:09:35 +0100

    Remove dependency on real ActionManager from test.
    ActionComponentChain can be passed a simple mocked ActionManager,
    instead of using a real ActionManager which requires half the
    client to be initialised (via TestMain).
    
    Change-Id: I294285d170e8facfa00e49683dd61068725d4f3e
    Reviewed-on: http://gerrit.dmdirc.com/2657
    Reviewed-by: Greg Holmes <greg@dmdirc.com>
    Automatic-Compile: DMDirc Build Manager

--- test/com/dmdirc/actions/ActionComponentChainTest.java
+++ test/com/dmdirc/actions/ActionComponentChainTest.java
@@ -22,24 +22,45 @@
 
 package com.dmdirc.actions;
 
-import com.dmdirc.TestMain;
 import com.dmdirc.Server;
 
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class ActionComponentChainTest {
 
-    @BeforeClass
-    public static void setUp() throws Exception {
-        TestMain.getTestMain();
+    @Mock private ActionManager actionManager;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        when(actionManager.getComponent("SERVER_NAME"))
+                .thenReturn(CoreActionComponent.SERVER_NAME);
+        when(actionManager.getComponent("SERVER_NETWORK"))
+                .thenReturn(CoreActionComponent.SERVER_NETWORK);
+        when(actionManager.getComponent("STRING_LENGTH"))
+                .thenReturn(CoreActionComponent.STRING_LENGTH);
+        when(actionManager.getComponent("STRING_STRING"))
+                .thenReturn(CoreActionComponent.STRING_STRING);
+        when(actionManager.getComponent("USER_MODES"))
+                .thenReturn(CoreActionComponent.USER_MODES);
+
+        // TODO: Testing for this behaviour here is odd - ActionComponentChain
+        //       should probably do its own validation and throw a more
+        //       appropriate exception.
+        when(actionManager.getComponent(""))
+                .thenThrow(new AssertionError());
     }
 
     @Test
     public void testSingle() {
-        final ActionComponentChain chain = new ActionComponentChain(String.class, "STRING_STRING");
+        final ActionComponentChain chain = new ActionComponentChain(String.class, "STRING_STRING", actionManager);
         assertEquals(chain.get("foo bar baz"), "foo bar baz");
         assertEquals("STRING_STRING", chain.toString());
     }
@@ -47,44 +68,44 @@ public class ActionComponentChainTest {
     @Test
     public void testDouble() {
         final ActionComponentChain chain = new ActionComponentChain(String.class,
-                "STRING_STRING.STRING_STRING");
+                "STRING_STRING.STRING_STRING", actionManager);
         assertEquals(chain.get("foo bar baz"), "foo bar baz");
         assertEquals("STRING_STRING.STRING_STRING", chain.toString());
     }
 
     @Test(expected=IllegalArgumentException.class)
     public void testInvalidName() {
-        new ActionComponentChain(String.class, "STRONG_STRING");
+        new ActionComponentChain(String.class, "STRONG_STRING", actionManager);
     }
 
     @Test(expected=IllegalArgumentException.class)
     public void testInvalidType() {
-        new ActionComponentChain(String.class, "USER_MODES.STRING_STRING");
+        new ActionComponentChain(String.class, "USER_MODES.STRING_STRING", actionManager);
     }
 
     @Test(expected=IllegalArgumentException.class)
     public void testInvalidLink() {
-        new ActionComponentChain(String.class, "STRING_STRING.USER_MODES");
+        new ActionComponentChain(String.class, "STRING_STRING.USER_MODES", actionManager);
     }
 
     @Test
     public void testAppliesTo() {
         final ActionComponentChain chain = new ActionComponentChain(String.class,
-                "STRING_STRING.STRING_STRING.STRING_LENGTH");
+                "STRING_STRING.STRING_STRING.STRING_LENGTH", actionManager);
         assertEquals(String.class, chain.appliesTo());
     }
 
     @Test
     public void testGetType() {
         final ActionComponentChain chain = new ActionComponentChain(String.class,
-                "STRING_STRING.STRING_STRING.STRING_LENGTH");
+                "STRING_STRING.STRING_STRING.STRING_LENGTH", actionManager);
         assertEquals(Integer.class, chain.getType());
     }
 
     @Test
     public void testGetName() {
         final ActionComponentChain chain = new ActionComponentChain(String.class,
-                "STRING_STRING.STRING_STRING.STRING_LENGTH");
+                "STRING_STRING.STRING_STRING.STRING_LENGTH", actionManager);
 
         assertTrue(chain.getName().indexOf(CoreActionComponent.STRING_STRING.getName()) > -1);
         assertTrue(chain.getName().indexOf(CoreActionComponent.STRING_LENGTH.getName()) > -1);
@@ -92,46 +113,46 @@ public class ActionComponentChainTest {
 
     @Test(expected=AssertionError.class)
     public void testEmptyAppliesTo() {
-        final ActionComponentChain chain = new ActionComponentChain(String.class, "");
+        final ActionComponentChain chain = new ActionComponentChain(String.class, "", actionManager);
         chain.appliesTo();
     }
 
     @Test(expected=AssertionError.class)
     public void testEmptyGetType() {
-        final ActionComponentChain chain = new ActionComponentChain(String.class, "");
+        final ActionComponentChain chain = new ActionComponentChain(String.class, "", actionManager);
         chain.getType();
     }
 
     @Test(expected=AssertionError.class)
     public void testEmptyGetName() {
-        final ActionComponentChain chain = new ActionComponentChain(String.class, "");
+        final ActionComponentChain chain = new ActionComponentChain(String.class, "", actionManager);
         chain.getName();
     }
 
     @Test(expected=AssertionError.class)
     public void testEmptyToString() {
-        final ActionComponentChain chain = new ActionComponentChain(String.class, "");
+        final ActionComponentChain chain = new ActionComponentChain(String.class, "", actionManager);
         chain.toString();
     }
 
     @Test()
     public void testRequiresConnection1() {
         final ActionComponentChain chain = new ActionComponentChain(Server.class,
-                "SERVER_NETWORK");
+                "SERVER_NETWORK", actionManager);
         assertTrue(chain.requiresConnection());
     }
 
     @Test()
     public void testRequiresConnection2() {
         final ActionComponentChain chain = new ActionComponentChain(Server.class,
-                "SERVER_NETWORK.STRING_LENGTH");
+                "SERVER_NETWORK.STRING_LENGTH", actionManager);
         assertTrue(chain.requiresConnection());
     }
 
     @Test()
     public void testRequiresConnection3() {
         final ActionComponentChain chain = new ActionComponentChain(Server.class,
-                "SERVER_NAME.STRING_LENGTH");
+                "SERVER_NAME.STRING_LENGTH", actionManager);
         assertFalse(chain.requiresConnection());
     }
 
