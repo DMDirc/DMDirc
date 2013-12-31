2d080cf150615a0094d4927094891c3d012df542
Shane Mc Cormack <shane@dmdirc.com>
2013-12-31 17:53:12 +0000

    Make UpdateCopyright.sh script a bit more intelligent.
    Change-Id: I88f633789679c6040268a83188004cf96a56037f
    Reviewed-on: http://gerrit.dmdirc.com/2975
    Automatic-Compile: DMDirc Build Manager
    Reviewed-by: Greg Holmes <greg@dmdirc.com>

--- UpdateCopyright.sh
+++ UpdateCopyright.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 # Update Copyright headers in all files.
 
+MONTH=`date +%m`
 THISYEAR=`date +%Y`
-THISYEAR=$((${THISYEAR} + 1))
 
-find . -regextype posix-egrep -iregex '.*/(.*\.(java|sh|php|xml|dpr|nsh|html|htm)|dmdirc.license|DMDirc-Apple.c|DMDirc - MIT|copyright)$' -exec sed -i 's/\(Copyright.*\)2006-20[0-9][0-9]\(.*DMDirc.*\)/\12006-'${THISYEAR}'\2/' {} \;
+if [ ${MONTH} -eq 12 ]; then
+	THISYEAR=$((${THISYEAR} + 1))
+elif [ ${MONTH} -ne 1 ]; then
+	echo "This script only makes sense being run near the start of a year..."
+	exit 1;
+fi;
+
+echo "Updating copyright to: ${THISYEAR}"
+
+find . -regextype posix-egrep -iregex '.*/(.*\.(java|sh|php|xml|dpr|nsh|html|htm)|dmdirc.license|LICENCE|DMDirc-Apple.c|DMDirc - MIT|copyright)$' -exec sed -i 's/\(Copyright.*\)2006-20[0-9][0-9]\(.*DMDirc.*\)/\12006-'${THISYEAR}'\2/' {} \;
