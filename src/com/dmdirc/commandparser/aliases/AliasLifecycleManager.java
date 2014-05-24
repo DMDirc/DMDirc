bd3583313c227ab84b9c9f7f90bc0192527e846f
Chris Smith <chris@dmdirc.com>
2014-05-24 14:48:46 +0100

    Add an AliasLoader and associated plumbing.
    Introduce a global lifecycle component interface which provides a
    way to start/stop components, and will help with The Quitting Problem™
    when we get around to it.
    
    Change-Id: I6d43f354e90e8206bea3f61a74d09728c9cac5aa
    Reviewed-on: http://gerrit.dmdirc.com/3520
    Reviewed-by: Greg Holmes <greg@dmdirc.com>
    Automatic-Compile: DMDirc Build Manager

---
+++ src/com/dmdirc/commandparser/aliases/AliasLifecycleManager.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2006-2014 DMDirc Developers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.commandparser.aliases;
+
+import com.dmdirc.interfaces.SystemLifecycleComponent;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * Manages the lifecycle of the alias system, loading at startup and saving at shutdown.
+ */
+@Singleton
+public class AliasLifecycleManager implements SystemLifecycleComponent {
+
+    /** The loader to use to load/save aliases. */
+    private final AliasLoader loader;
+
+    @Inject
+    public AliasLifecycleManager(final AliasLoader loader) {
+        this.loader = loader;
+    }
+
+    @Override
+    public void startUp() {
+        loader.load();
+    }
+
+    @Override
+    public void shutDown() {
+        loader.save();
+    }
+
+}
