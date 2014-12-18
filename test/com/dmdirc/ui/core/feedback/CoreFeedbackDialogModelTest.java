6100420580c936c7db124d324ebd15f281b838ab
Greg Holmes <greg@dmdirc.com>
2014-12-18 18:53:18 +0000

    Created FeedbackHelper class.

--- test/com/dmdirc/ui/core/feedback/CoreFeedbackDialogModelTest.java
+++ test/com/dmdirc/ui/core/feedback/CoreFeedbackDialogModelTest.java
@@ -22,17 +22,8 @@
 
 package com.dmdirc.ui.core.feedback;
 
-import com.dmdirc.ServerState;
-import com.dmdirc.interfaces.Connection;
-import com.dmdirc.interfaces.ConnectionManager;
-import com.dmdirc.interfaces.config.AggregateConfigProvider;
 import com.dmdirc.interfaces.ui.FeedbackDialogModelListener;
-import com.dmdirc.parser.interfaces.Parser;
-import com.dmdirc.util.ClientInfo;
 
-import com.google.common.collect.Lists;
-
-import java.nio.file.Path;
 import java.util.Optional;
 
 import org.junit.Before;
@@ -49,70 +40,22 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class CoreFeedbackDialogModelTest {
 
-    @Mock private AggregateConfigProvider config;
-    @Mock private ConnectionManager connectionManager;
-    @Mock private FeedbackSenderFactory feedbackSenderFactory;
-    @Mock private FeedbackSender feedbackSender;
-    @Mock private FeedbackDialogModelListener listener;
-    @Mock private Path path;
-    @Mock private ClientInfo clientInfo;
-    @Mock private Connection connection;
-    @Mock private Parser parser;
     private static final String NAME = "Bob Dole";
     private static final String EMAIL = "bob@dole.com";
     private static final String FEEDBACK = "DMDirc Rocks.";
-    private static final String VERSION = "0.1";
-    private static final String PATH = "/path/to/config";
-    private static final String JAVA = "javaInfo";
-    private static final String OS = "osInfo";
-    private static final String SERVER_NAME = "serverName";
-    private static final String NETWORK_NAME = "networkName";
-    private static final String SERVER_SOFTWARE = "serverSoftware";
-    private static final String SERVER_SOFTWARE_TYPE = "serverSoftwareType";
-    private static final String BOOLEAN_MODES = "booleanModes";
-    private static final String LIST_MODES = "listChannelModes";
-    private static final String PARAM_MODES = "paramModes";
-    private static final String DOUBLE_PARAM_MODES = "doubleParamModes";
+
+    @Mock private FeedbackSenderFactory feedbackSenderFactory;
+    @Mock private FeedbackSender feedbackSender;
+    @Mock private FeedbackDialogModelListener listener;
+    @Mock private FeedbackHelper feedbackHelper;
+
     private CoreFeedbackDialogModel instance;
-
-    private String getServerInfoOutput() {
-        return "Actual name: " + SERVER_NAME + '\n'
-                + "Network: " + NETWORK_NAME + '\n'
-                + "IRCd: " + SERVER_SOFTWARE + " - " + SERVER_SOFTWARE_TYPE + '\n'
-                + "Modes: " + BOOLEAN_MODES + ' ' + LIST_MODES + ' ' + PARAM_MODES + ' '
-                + DOUBLE_PARAM_MODES;
-    }
-
-    private String getDMDircInfoOutput() {
-        return "DMDirc version: " + VERSION + '\n'
-                + "Profile directory: " + PATH + '\n'
-                + "Java version: " + JAVA + '\n'
-                + "OS Version: " + OS;
-    }
 
     @Before
     public void setup() {
-        instance = new CoreFeedbackDialogModel(config,
-                connectionManager, feedbackSenderFactory, path, clientInfo);
-        when(connectionManager.getConnections()).thenReturn(Lists.newArrayList());
+        instance = new CoreFeedbackDialogModel(feedbackSenderFactory, feedbackHelper);
         when(feedbackSenderFactory.getFeedbackSender(anyString(), anyString(), anyString(),
                 anyString(), anyString(), anyString())).thenReturn(feedbackSender);
-        when(config.getOption("version", "version")).thenReturn(VERSION);
-        when(clientInfo.getVersionInformation()).thenReturn(VERSION);
-        when(path.toString()).thenReturn(PATH);
-        when(clientInfo.getJavaInformation()).thenReturn(JAVA);
-        when(clientInfo.getOperatingSystemInformation()).thenReturn(OS);
-        when(connectionManager.getConnections()).thenReturn(Lists.newArrayList(connection));
-        when(connection.getParser()).thenReturn(parser);
-        when(parser.getServerName()).thenReturn(SERVER_NAME);
-        when(connection.getState()).thenReturn(ServerState.CONNECTED);
-        when(connection.getNetwork()).thenReturn(NETWORK_NAME);
-        when(parser.getServerSoftware()).thenReturn(SERVER_SOFTWARE);
-        when(parser.getServerSoftwareType()).thenReturn(SERVER_SOFTWARE_TYPE);
-        when(parser.getBooleanChannelModes()).thenReturn(BOOLEAN_MODES);
-        when(parser.getListChannelModes()).thenReturn(LIST_MODES);
-        when(parser.getParameterChannelModes()).thenReturn(PARAM_MODES);
-        when(parser.getDoubleParameterChannelModes()).thenReturn(DOUBLE_PARAM_MODES);
     }
 
     @Test
@@ -151,35 +94,6 @@ public class CoreFeedbackDialogModelTest {
     }
 
     @Test
-    public void testGetDMDircInfo() {
-        assertEquals(getDMDircInfoOutput(), instance.getDMDircInfo());
-    }
-
-    @Test
-    public void testGetServerInfo_Single() {
-        assertEquals(getServerInfoOutput() + '\n', instance.getServerInfo());
-    }
-
-    @Test
-    public void testGetServerInfo_Multiple() {
-        when(connectionManager.getConnections()).thenReturn(Lists.newArrayList(connection,
-                connection));
-        assertEquals(getServerInfoOutput() + '\n' + getServerInfoOutput() + '\n',
-                instance.getServerInfo());
-    }
-
-    @Test
-    public void testGetServerInfoConnection() {
-        assertEquals(getServerInfoOutput(), instance.getServerInfo(connection));
-    }
-
-    @Test
-    public void testGetServerInfoConnection_Disconnected() {
-        when(connection.getState()).thenReturn(ServerState.DISCONNECTED);
-        assertEquals("", instance.getServerInfo(connection));
-    }
-
-    @Test
     public void testSaveWithoutServerWithoutDMDirc() {
         instance.setIncludeDMDircInfo(false);
         instance.setIncludeServerInfo(false);
@@ -187,7 +101,8 @@ public class CoreFeedbackDialogModelTest {
         instance.setEmail(Optional.ofNullable(EMAIL));
         instance.setFeedback(Optional.ofNullable(FEEDBACK));
         instance.save();
-        verify(feedbackSenderFactory).getFeedbackSender(NAME, EMAIL, FEEDBACK, VERSION, "", "");
+        verify(feedbackSenderFactory).getFeedbackSender(NAME, EMAIL, FEEDBACK,
+                feedbackHelper.getVersion(), "", "");
     }
 
     @Test
@@ -198,8 +113,8 @@ public class CoreFeedbackDialogModelTest {
         instance.setEmail(Optional.ofNullable(EMAIL));
         instance.setFeedback(Optional.ofNullable(FEEDBACK));
         instance.save();
-        verify(feedbackSenderFactory).getFeedbackSender(NAME, EMAIL, FEEDBACK, VERSION,
-                "", getDMDircInfoOutput());
+        verify(feedbackSenderFactory).getFeedbackSender(NAME, EMAIL, FEEDBACK,
+                feedbackHelper.getVersion(), "", feedbackHelper.getDMDircInfo());
     }
 
     @Test
@@ -210,8 +125,8 @@ public class CoreFeedbackDialogModelTest {
         instance.setEmail(Optional.ofNullable(EMAIL));
         instance.setFeedback(Optional.ofNullable(FEEDBACK));
         instance.save();
-        verify(feedbackSenderFactory).getFeedbackSender(NAME, EMAIL, FEEDBACK, VERSION,
-                getServerInfoOutput() + '\n', "");
+        verify(feedbackSenderFactory).getFeedbackSender(NAME, EMAIL, FEEDBACK,
+                feedbackHelper.getVersion(), feedbackHelper.getServerInfo(), "");
     }
 
     @Test
@@ -222,8 +137,9 @@ public class CoreFeedbackDialogModelTest {
         instance.setEmail(Optional.ofNullable(EMAIL));
         instance.setFeedback(Optional.ofNullable(FEEDBACK));
         instance.save();
-        verify(feedbackSenderFactory).getFeedbackSender(NAME, EMAIL, FEEDBACK, VERSION,
-                getServerInfoOutput() + '\n', getDMDircInfoOutput());
+        verify(feedbackSenderFactory).getFeedbackSender(NAME, EMAIL, FEEDBACK,
+                feedbackHelper.getVersion(), feedbackHelper.getServerInfo(),
+                feedbackHelper.getDMDircInfo());
     }
 
     @Test
