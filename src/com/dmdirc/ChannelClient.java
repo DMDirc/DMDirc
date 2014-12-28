845acfce20c0048ef098ba91837efea4d10236b9
Greg Holmes <greg@dmdirc.com>
2014-12-28 17:36:05 +0000

    Start work on GroupChatUser implementation.

---
+++ src/com/dmdirc/ChannelClient.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2006-2014 DMDirc Developers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc;
+
+import com.dmdirc.interfaces.GroupChat;
+import com.dmdirc.interfaces.GroupChatUser;
+import com.dmdirc.interfaces.User;
+
+/**
+ *
+ */
+public class ChannelClient implements GroupChatUser {
+
+    private final User user;
+    private final GroupChat groupChat;
+
+    public ChannelClient(final User user, final GroupChat groupChat) {
+        this.user = user;
+        this.groupChat = groupChat;
+    }
+
+    @Override
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public GroupChat getGroupChat() {
+        return groupChat;
+    }
+
+    @Override
+    public String getAllModes() {
+        // TODO: Implement this.
+        return "";
+    }
+}
