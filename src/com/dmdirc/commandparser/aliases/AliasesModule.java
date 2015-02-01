8dccc60cf504f86f892cb3a9d18d1f71f64f24ee
Chris Smith <chris@dmdirc.com>
2015-02-01 15:12:59 +0000

    Remove some more AppErrorEvents.

--- src/com/dmdirc/commandparser/aliases/AliasesModule.java
+++ src/com/dmdirc/commandparser/aliases/AliasesModule.java
@@ -22,7 +22,6 @@
 
 package com.dmdirc.commandparser.aliases;
 
-import com.dmdirc.DMDircMBassador;
 import com.dmdirc.commandline.CommandLineOptionsModule.Directory;
 import com.dmdirc.commandline.CommandLineOptionsModule.DirectoryType;
 import com.dmdirc.interfaces.Migrator;
@@ -60,9 +59,8 @@ public class AliasesModule {
     }
 
     @Provides(type = Provides.Type.SET)
-    public Migrator getDefaultsMigrator(@Directory(DirectoryType.BASE) final Path directory,
-            final DMDircMBassador eventBus) {
-        return new DefaultAliasInstaller(directory.resolve("aliases.yml"), eventBus);
+    public Migrator getDefaultsMigrator(@Directory(DirectoryType.BASE) final Path directory) {
+        return new DefaultAliasInstaller(directory.resolve("aliases.yml"));
     }
 
 }
