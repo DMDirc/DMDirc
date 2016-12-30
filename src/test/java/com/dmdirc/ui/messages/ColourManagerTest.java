82f897f85e6eafea0faaaec86597dfe54b4182dd
Chris Smith <chris87@gmail.com>
2016-12-30 23:14:38 +0000

    Add an interface for ColourManager.

--- src/test/java/com/dmdirc/ui/messages/ColourManagerTest.java
+++ src/test/java/com/dmdirc/ui/messages/ColourManagerTest.java
@@ -57,7 +57,7 @@ public class ColourManagerTest {
 
     @Before
     public void setup() {
-        manager = new ColourManager(configManager);
+        manager = new ColourManagerImpl(configManager);
         verify(configManager).addChangeListener(anyString(), configListener.capture());
     }
 
