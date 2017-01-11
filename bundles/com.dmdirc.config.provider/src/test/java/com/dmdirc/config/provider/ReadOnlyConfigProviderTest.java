0c4a027b42df661a13e8d856ac47f3cb1dc2215a
Chris Smith <chris@chameth.com>
2017-01-11 17:33:36 +0000

    Move config provider tests to the right place.

---
+++ bundles/com.dmdirc.config.provider/src/test/java/com/dmdirc/config/provider/ReadOnlyConfigProviderTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2006-2017 DMDirc Developers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.dmdirc.config.provider;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReadOnlyConfigProviderTest {
+
+    private final TestReadOnlyConfigProvider s = new TestReadOnlyConfigProvider();
+
+    @Test
+    public void testGetBoolean() {
+        assertTrue(s.getOptionBool("true", "true"));
+        assertFalse(s.getOptionBool("true", "false"));
+    }
+
+    @Test
+    public void testGetInt() {
+        assertEquals(42, s.getOptionInt("true", "42").intValue());
+    }
+
+    @Test
+    public void testGetList() {
+        assertTrue(s.getOptionList("false", "moo").isEmpty());
+        assertTrue(s.getOptionList("true", "").isEmpty());
+        assertTrue(s.getOptionList("true", "\n\n\n").isEmpty());
+        assertEquals(4, s.getOptionList("true", "\n\n\na", false).size());
+        assertEquals(4, s.getOptionList("true", "a\nb\nc\nd", true).size());
+        assertEquals("c", s.getOptionList("true", "a\nb\nc\nd", true).get(2));
+    }
+
+}
